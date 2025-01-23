c736d8f4d49ee76558a74ed1599843ad6f1b6fcc
BertaAsensio <bertagonzalez224@gmail.com>
2025-01-23 09:13:55 +0100

    add hangman doc #4

---
+++ staff/berta-asensio/playground/hangman/main.js
@@ -0,0 +1,38 @@
+````
+console.clear()
+
+var word = 'murcielago'
+var matches = []
+
+for (var i = 0; i < word.length; i++)
+    matches[i] = ' _ '
+
+var attemps = 0
+var maxAttemps = 6
+var guessCharacter = ''
+
+function printMatches() {
+    var matchesString = ''
+
+    for (var i = 0; i < matches.length; i++)
+        matchesString += matches[i] + ''
+
+    console.log(matchesString)
+}
+
+function askCharacter() {
+    guessCharacter = prompt('character?')
+}
+
+function checkGuessCharacterMatches() {
+//TODO
+    
+}
+
+console.log('...')
+
+printMatches()
+askCharacter()
+checkGuessCharacterMatches()
+printMatches()
+````
