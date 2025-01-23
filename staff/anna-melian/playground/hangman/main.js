edd9a6ac850a63e73e7415d22b44f00f79832aee
Ans1011 <ammercade@gmail.com>
2025-01-23 15:40:50 +0100

    add hangman base code #13

---
+++ staff/anna-melian/playground/hangman/main.js
@@ -0,0 +1,36 @@
+console.clear()
+// var word = prompt ('word?')
+var word = "murcielago"
+var matches = []
+
+for (var i = 0; i < word.length; i++)
+    matches[i] = "_"
+
+var attemps = 0
+var maxAttemps = 6
+var guessCharacter = ""
+
+function printMatches() {
+    var matchesString = ""
+
+    for (var i = 0; i < matches.length; i++)
+        matchesString += matches[i] + " "
+
+    console.log(matchesString)
+}
+
+function askCharacter() {
+    guessCharacter = prompt("character?")
+
+    
+}
+
+function checkGuessCharacterMatches() {
+    
+    
+}
+
+console.log("...")
+printMatches()
+askCharacter()
+checkGuessCharacterMatches()
