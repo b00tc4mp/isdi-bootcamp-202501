a255d0c2e1d2746a5eac0bc14fb814e1464f5956
Santiagon1996 <santiagoanardelli96@gmail.com>
2025-01-23 16:53:22 -0300

    add my hangman game WIP #17

--- staff/santiago-nardelli/playground/JS/hangman.js
+++ staff/santiago-nardelli/playground/JS/hangman.js
@@ -1,35 +1,112 @@
-
-var word = 'murcielago'
+//Juego del ahorcado
+// 1. Definir una palabra
+var word = 'batman'
+// 2. Definir un array de caracteres
 var matches = []
+// 3. Definir un contador de intentos
+var attempts = 0
+// 4. Definir un máximo de intentos
+var maxAttempts = 6
+// 5. Definir un carácter a adivinar como un string vacío
+var guessCharacter = ''
 
+// 1 Paso numero 1 mediante un for recorrer la palabra y agregar un guion bajo por cada letra
+// convierto las posiciones del array a un _ por la lenght de la word
 for (var i = 0; i < word.length; i++)
     matches[i] = '_'
 
-var attemps = 0
-var maxAttemps = 6
-var guessCharacter = ''
-
 function printMatches() {
-    var matchesString = ''
+    // Inicializamos una variable vacía para almacenar el string que vamos a construir 
+    // lo que buscamos aqui es pasar de un array  de "_ "_" "_"_" _" a un string "_ _ _ _ _"
+    var matchesString = '';
 
-    for (var i = 0; i < matches.length; i++)
-        matchesString += matches[i] + ' '
+    // Recorremos el array 'matches' que contiene los "_ "_" "_"_" _" y lo almacenamos en 'matchesString'
+    for (var i = 0; i < matches.length; i++) {
+        // Añadimos cada carácter del array 'matches' seguido de un espacio a 'matchesString'
 
-    console.log(matchesString)
+        matchesString += matches[i] + ' ';//esto es lo que se va a imprimir en la consola
+        // Ejemplo: si matches = ['_','_','_','_','_'] entonces matchesString = '_ _ _ _ _ '
+    }
+
+    // Imprimimos el string resultante en la consola
+    console.log(matchesString);
 }
 
-function askCharacter() {
-    guessCharacter = prompt('character?')
+
+
+// 2  mediante un prompt pedir al usuario que ingrese un caracter
+function askCharacter() { 
+    guessCharacter = prompt('Ingrese un carácter:');
+    
 }
 
-function checkGuessCharacterMatches() {
-    // TODO
+
+
+// 3 mediante un for recorrer la palabra y comparar si el caracter ingresado por el usuario es igual a alguno de los caracteres de la palabra
+// si es igual reemplazar el guion bajo por el caracter ingresado
+function checkGuessCharacterMatches(){
+    // Inicializamos una variable 'check' en false
+    var check = false
+    // Recorremos la palabra letra por letra
+    for (var i = 0; i < word.length; i++) {
+        // Si la letra de la palabra es igual al caracter ingresado por el usuario
+        // Reemplazar el guión bajo por la letra en la misma posición
+        // Cambiar el valor de 'check' a true 
+        if(guessCharacter == word[i]){
+            // Reemplazamos el guión bajo por la letra en la misma posición
+            matches[i] = word[i]
+            // Cambiamos el valor de 'check' a true
+            check = true
+            alert('Cathed')
+        }
+        
+        
+    }
+
+   
+    // Si 'check' es false, incrementamos el contador de intentos
+    if (check=== false){
+        alert('One life down')
+        maxAttempts--
+        
+    }
 }
 
+function hasPlayerWon() {
+    // Verificar si todos los caracteres de 'matches' coinciden con los de 'word'
+    // Si todos los caracteres coinciden, retornar true
+    for (var i = 0; i < word.length; i++) {
+        // Si hay alguna letra que no coincida, retornar false
+        // Si todas las letras coinciden, retornar true
+        // Ejemplo: si matches = ['b','a','t','m','a','n'] y word = 'batman' entonces retornar true
+        // Ejemplo: si matches = ['b','a','t','m','a','n'] y word = 'batmen' entonces retornar false 
+        if (matches[i] !== word[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
-console.log('...')
 
 printMatches()
-askCharacter()
-checkGuessCharacterMatches()
-printMatches()
+// Bucle principal del juego
+//Mientras los attemps sean menores a maxAttempts
+while(attempts < maxAttempts ){
+
+    askCharacter()
+    checkGuessCharacterMatches()
+    printMatches()
+
+    // Verificar si el jugador ha ganado
+    if ( hasPlayerWon()) {
+        console.log('¡Felicidades! Has adivinado la palabra.');
+        break;
+    }
+
+    // Verificar si el jugador ha perdido
+    if (attempts >= maxAttempts) {
+        console.log('No tienes más intentos. La palabra era: ' + word);
+        break;
+    }
+}
+
