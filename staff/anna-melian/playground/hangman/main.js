cfd88731aa790f722447874829379c427294dc54
Ans1011 <ammercade@gmail.com>
2025-01-23 18:06:23 +0100

    updated hangman #13

--- staff/anna-melian/playground/hangman/main.js
+++ staff/anna-melian/playground/hangman/main.js
@@ -1,6 +1,5 @@
 console.clear()
-// var word = prompt ('word?')
-var word = "murcielago"
+var word = prompt ('Choose a secret word:')
 var matches = []
 
 for (var i = 0; i < word.length; i++)
@@ -9,6 +8,7 @@ for (var i = 0; i < word.length; i++)
 var attemps = 0
 var maxAttemps = 6
 var guessCharacter = ""
+var equalCharacters = 0
 
 function printMatches() {
     var matchesString = ""
@@ -20,17 +20,51 @@ function printMatches() {
 }
 
 function askCharacter() {
-    guessCharacter = prompt("character?")
+    guessCharacter = prompt("Choose a character")
 
     
 }
+
+
 
 function checkGuessCharacterMatches() {
-    
+    var correct = 0
+    for (i = 0; i < word.length; i++) {
+        if (guessCharacter!=word[i]) {
+            
+            
+            }
+        else {
+            correct++
+            equalCharacters++
+            matches[i] = guessCharacter
+            
+        }
+        
+    }
+    if (correct != 0) {
+        printMatches()
+        console.log("Correct!")
+    }
+    else {
+        console.log("Wrong!")
+        attemps++
+    }
     
 }
 
+function asks() {
+    while (equalCharacters!=word.length) {
+        if (attemps!=maxAttemps) {
+        askCharacter()
+        checkGuessCharacterMatches()
+        }
+        else {
+        console.log("You lose!" + " The secret word was " + word)
+        }
+    }
+    console.log("Congratulations! You win!!!")
+}
 console.log("...")
 printMatches()
-askCharacter()
-checkGuessCharacterMatches()
+asks()
