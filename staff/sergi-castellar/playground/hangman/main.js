3f5d14c28a149990c60016924ea3bb0d040a406c
Sergi <sergicpiqueras@gmail.com>
2025-01-23 15:46:45 +0100

    add possibly try directly the word/accept only one letter #5

--- staff/sergi-castellar/playground/hangman/main.js
+++ staff/sergi-castellar/playground/hangman/main.js
@@ -1,22 +1,22 @@
 console.clear()
 
-//var word = 'ura' //palabra a adivinar
+
 var word = '' //palabra a adivinar
 var matches = [] // array de almacenaje de '_' y letras adivinadas
 var attempts = 0 //intentos actuales
 var maxAttempts = 6 //intentos maximos
 var guessCharacter = '' //intento de letra dada
-var charactersChecked = []
-var correctCharacters = 0
+var charactersChecked = [] // array de letras ya introducidas
+var correctCharacters = 0 // letras de la palabra que llevas adivinadas
 
-function askWord() {
+function askWord() { // pregunta por prompt la palabra a adivinar
     var localWord = prompt('Introduce la palabra a adivinar')
     word = localWord
     initialLowBarArray()
 }
 
-function initialLowBarArray() {
-    for (var i = 0; i < word.length; i++){ //bucle para mostrar primero los '_'
+function initialLowBarArray() { // funcion para mostrar primero los '_'
+    for (var i = 0; i < word.length; i++){ 
     matches[i] = '_'
     }
 }
@@ -26,7 +26,56 @@ function askCharacter() { // funcion que pregunta input por prompt y retorna el 
     checkGuessCharacterMatches(guessCharacter)
 }
 
-function checkIfChecked(guessCharacter) {
+function checkGuessCharacterMatches(guessCharacter) { //comparar letra dada con cada caracter de la palabra
+    if (onlyOneSimpleLetter(guessCharacter) === true) {
+        if (checkIfChecked(guessCharacter) === false) {
+            charactersChecked.push(guessCharacter)
+            var isCorrect = false
+            for (var i = 0; i < word.length; i++) {
+                if (guessCharacter === word[i]) {
+                    matches[i] = guessCharacter
+                    correctCharacters += 1
+                    isCorrect = true
+                }
+            }
+            if (isCorrect === false) { // si la letra no coincide con ninguna de las letras de la palabra, 
+                attempts += 1 // suma un intento a attempts
+                alert('Error, llevas ' + attempts + ' intentos de ' + maxAttempts + '\n' + charactersChecked) // retorna alert con los intentos
+            } else {
+                if (correctCharacters === word.length) {
+                    alert('¡Has ganado!. Efectivamente, la palabra era ' + word + '. Volvamos a jugar')
+                    resetGame()
+                } else {
+                    alert('¡Vas bien!\n' + matches)
+                }
+            }
+            if (attempts < maxAttempts) { //si no has perdido sigues jugando
+                askCharacter()
+            } else {
+                alert('Has perdido.' + attempts + ' intentos de ' + maxAttempts + '.\nLa palabra era ' + word + '. Volvamos a jugar :)')
+                resetGame()
+            }
+
+        } else {
+            alert('Esa ya la has dicho, que no te enteras. Sigues con ' + attempts + ' intentos de ' + maxAttempts + '\n' + matches)
+            askCharacter()
+        }
+    } else if (isThisTheResult(guessCharacter) === true) {
+        if (guessCharacter === word) {
+            alert('¡Has ganado!. Efectivamente, la palabra era ' + word + '. Volvamos a jugar')
+                    resetGame()
+        } else {
+            alert('Has perdido por querer adelantarte ;)\nLa palabra era ' + word + '. Volvamos a jugar :)')
+                resetGame()
+        }
+        
+    } else {
+        alert('Solo se acepta una letra minúscula de la a a la z sin ningún simbolo. Si ya tienes la respuesta, prueba a añadir una palabra de ' + word.length + ' letras.')
+        askCharacter()
+    }  
+}
+
+function checkIfChecked(guessCharacter) { // mira si esa letra ya ha sido introducida en esta partida
     var isChecked = false
     for (var i = 0; i < charactersChecked.length; i++) {
         if (guessCharacter === charactersChecked[i]) {
@@ -36,42 +85,27 @@ function checkIfChecked(guessCharacter) {
     return isChecked
 }
 
-function checkGuessCharacterMatches(guessCharacter) { //comparar letra dada con cada caracter de la palabra
-    if (checkIfChecked(guessCharacter) === false) {
-        charactersChecked.push(guessCharacter)
-        var isCorrect = false
-        for (var i = 0; i < word.length; i++) {
-            if (guessCharacter === word[i]) {
-                matches[i] = guessCharacter
-                correctCharacters += 1
-                isCorrect = true
-            }
-        }
-        if (isCorrect === false) { // si la letra no coincide con ninguna de las letras de la palabra, 
-            attempts += 1 // suma un intento a attempts
-            alert('Error, llevas ' + attempts + ' intentos de ' + maxAttempts + '\n' + charactersChecked) // retorna alert con los intentos
-        } else {
-            if (correctCharacters === word.length) {
-                alert('¡Has ganado!. Efectivamente, la palabra era ' + word + ' Volvamos a jugar')
-                resetGame()
-            } else {
-                alert('¡Vas bien!\n' + matches)
-            }
-        }
-        if (attempts < maxAttempts) { //si no has perdido sigues jugando
-            askCharacter()
-        } else {
-            alert('Has perdido.' + attempts + ' intentos de ' + maxAttempts + '.\nLa palabra era ' + word + '. Volvamos a jugar :)')
-            resetGame()
-        }
-
+function onlyOneSimpleLetter(input) {
+    var regex = /^[a-z]$/
+    if (regex.test(input)){
+        return true
     } else {
-        alert('Esa ya la has dicho, que no te enteras. Sigues con ' + attempts + ' intentos de ' + maxAttempts + '\n' + matches)
-        askCharacter()
+        return false
     }
 }
 
-function resetGame() {
+function isThisTheResult(input) {
+    var n = word.length
+    const regex = new RegExp(`^[a-z]{${n}}$`);
+    
+    if (regex.test(input)){
+        return true
+    } else {
+        return false
+    }
+}
+
+function resetGame() { // resetea variables para volver a empezar
     word = ''
     guessCharacter = ''
     attempts = 0
@@ -84,5 +118,3 @@ function resetGame() {
 
 askWord()
 askCharacter()
-
-
