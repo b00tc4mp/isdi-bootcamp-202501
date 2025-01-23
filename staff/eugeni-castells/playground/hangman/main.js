66e9c7523564d559d42d3326518bcd2bbd82d087
Eugeni <ecastellsricart@gmail.com>
2025-01-23 20:24:53 +0100

    add repeatedErrorMessage; errorArr push repeated errors fixed #7

--- staff/eugeni-castells/playground/hangman/main.js
+++ staff/eugeni-castells/playground/hangman/main.js
@@ -9,11 +9,14 @@ var maxAttempts = 6;
 var repeatedChaMessage = "";
 var resetGame = false;
 var oneCha = "";
+var hangMessage = "";
+var repeatedErrorMessage = "";
 function askWord() {
   word = prompt(`What is the secret word?`);
 }
 
 function guessCharacter() {
+  repeatedErrorMessage = "";
   var match = false;
   while (oneCha !== "yes" && oneCha !== "no") {
     oneCha = prompt("You wanna guess the whole word? Write 'yes' or 'no'");
@@ -35,6 +38,7 @@ function guessCharacter() {
         match = true;
       } else if (cha === word[i] && cha === matches[i]) {
         repeatedChaMessage = "Oops, looks like the letter was already guessed.";
+        match = true;
       }
     }
   } else if (oneCha === "yes" || oneCha === "Yes") {
@@ -44,39 +48,79 @@ function guessCharacter() {
   }
   if (match === false) {
     attempts++;
-    errorArr.push(cha);
+    if (errorArr.length === 0) {
+      errorArr.push(cha);
+    } else {
+      for (let i = 0; i < errorArr.length; i++) {
+        if (errorArr[i] !== cha) {
+          errorArr.push(cha);
+        } else
+          repeatedChaMessage =
+            "You already said that letter and it's wrong. Focus.";
+      }
+    }
+
+    //hangMan()
   }
   oneCha = "";
 }
-
+/*function hangMan(){
+  switch(attempts){
+    case 0:
+      break;
+    case 1:
+      hangMessage=`೦`
+      break;
+    case 2:
+      hangMessage=`ႃ`
+      break;
+    case 3:
+      hangMessage=`_ႃ`
+        break;
+    case 4:
+    hangMessage=`_ႃ_`
+    case 5:
+    hangMessage=`_ႃ_ \n
+                  /`
+    break;
+    case 6:
+      hangMessage=`_ႃ_ \n /\``
+      break;
+    default:
+      break;
+  }
+}*/
 function isGameWon() {
   if (guessedMatches === word.length) {
     alert(
       `Congrats, you won! The word was ${word}. You had ${
         maxAttempts - attempts
-      } attempts remaining. What a beast!`
+      } attempts remaining. What a beast!
+      `
     );
     attempts = 6;
   } else if (guessedMatches < word.length && attempts < maxAttempts) {
     printAttempts();
   } else {
-    alert(`Oh no, you lost :( The word was ${word}`);
+    alert(`Oh no, you lost :( The word was ${word}   ${hangMessage}`);
   }
 }
 function printAttempts() {
   if (maxAttempts - attempts > 1) {
-    alert(`${repeatedChaMessage} ${
+    alert(`${repeatedChaMessage} ${repeatedErrorMessage} ${
       maxAttempts - attempts
     } attempts remaining, keep trying!.
     Your progress: ${matchString}
     Errors: ${errorArr}
+    ${hangMessage}
     `);
   } else if (maxAttempts - attempts === 1) {
-    alert(`${repeatedChaMessage} ${
+    alert(`${repeatedChaMessage} ${repeatedErrorMessage} ${
       maxAttempts - attempts
     } attempt remaining. Be careful!
       Your progress: ${matchString}
       Errors: ${errorArr}
+      ${hangMessage}
        `);
   }
   repeatedChaMessage = "";
