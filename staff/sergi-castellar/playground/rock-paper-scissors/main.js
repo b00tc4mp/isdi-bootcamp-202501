29e5f2ee78e0f34c155d84f7242ef678e904bd76
Sergi <sergicpiqueras@gmail.com>
2025-01-25 19:47:26 +0100

    add n games function WIP #5

--- staff/sergi-castellar/playground/rock-paper-scissors/main.js
+++ staff/sergi-castellar/playground/rock-paper-scissors/main.js
@@ -1,18 +1,48 @@
 console.clear()
 
-var possiblePlays = ['rock', 'paper', 'scissors']
+var possiblePlays = ['rock 👊', 'paper 🤚', 'scissors ✌️']
+var numberOfGames = 0 // total de partidas que el jugador ha elegido jugar
+var numberOfGamesPlayed = 0 // contador del numero de partidas que se llevan
+var gamesWonPlayer = 0 // contador de las partidas ganadas que lleva el jugador en esta ronda de partidas
+var gamesWonCpu = 0 // contador de las partidas ganadas que lleva la cpu en esta ronda de partidas
+
+function chooseNumberOfMatches() {
+    numberOfGames = prompt('Welcome to the classic game of Rock, Paper, Scissors! How many rounds do you want to play?')
+    if (isAPositiveNumber(numberOfGames)) {
+        playTurn()
+    } else {
+        alert('Please, enter a valid positive number')
+        chooseNumberOfMatches()
+    }
+}
+
+function wasItTheLastGame() {
+    playTurn()
+
+}
 
 function playTurn() {
-    var playerInput = prompt(`Welcome to the classic game of Rock, Paper, Scissors! It's your turn: choose your move by entering one of the following options.\n- r for 'Rock'\n- p for 'Paper'\n- s for 'Scissors'\nReady to play?`).toLowerCase()
+    var playerInput = prompt(`It's your turn: choose your move by entering one of the following options.\n- r for 'Rock'👊\n- p for 'Paper'🤚\n- s for 'Scissors'✌️\nReady to play?`).toLowerCase()
     var playerPlay = ''
     var cpuPlay = ''
     var gameResult= ''
     if (isValidPlay(playerInput)){
+        numberOfGamesPlayed ++
         playerPlay = possiblePlays[convertPlayerInput(playerInput)] 
         cpuPlay = possiblePlays[generateFromZeroToTwo()]
         gameResult = evaluateResult(playerPlay, cpuPlay)
-        alert(selectEndingPhrase(gameResult) + `\n\nYour play: ${playerPlay}\n\nCPU play: ${cpuPlay}\n\nWinner: ${gameResult}`)
-        playTurn()
+        if (gameResult === 'player') {
+            gamesWonPlayer ++
+        } else if (gameResult === 'cpu') {
+            gamesWonCpu ++
+        }
+        alert(selectEndingPhrase(gameResult) + `\n\nYour play: ${playerPlay}\n\nCPU play: ${cpuPlay}\n\nWinner: ${gameResult}\n\nYou won ${gamesWonPlayer} games\n\nCpu won ${gamesWonCpu} matches`)
+        if (numberOfGamesPlayed < numberOfGames) {
+            playTurn()
+        } else {
+
+            alert(`End of the agreed-upon ${numberOfGames} rounds. The winner is ${winner}`) /// falta hacer funcion q diga quien gana la ronda de n partidas
+        }
     } else {
         alert("Please, only type 'r', 'p' or 's' in order to play the game")
         playTurn()
@@ -20,12 +50,11 @@ function playTurn() {
 }
 
 function selectEndingPhrase(gameResult) {
-switch (gameResult) {
-    case 'nobody': return "It's a tie! You both chose the same. Try again!"
-    case 'player': return "Congratulations, you won! You got lucky this time."
-    case 'cpu': return "The CPU wins this round. Don't give up, try again!"
-}
-
+    switch (gameResult) {
+        case 'nobody': return "It's a tie! You both chose the same. Try again!"
+        case 'player': return "Congratulations, you won! You got lucky this time."
+        case 'cpu': return "The CPU wins this round. Don't give up, try again!"
+    }
 }
 
 function evaluateResult(playerPlay, cpuPlay) {
@@ -34,15 +63,15 @@ function evaluateResult(playerPlay, cpuPlay) {
         result = 'nobody'
     } // empate
 
-    else if (playerPlay === 'rock' && cpuPlay === 'paper' ||
-    playerPlay === 'paper' && cpuPlay === 'scissors' ||
-    playerPlay === 'scissors' && cpuPlay === 'rock') {
+    else if (playerPlay === 'rock 👊' && cpuPlay === 'paper 🤚' ||
+    playerPlay === 'paper 🤚' && cpuPlay === 'scissors ✌️' ||
+    playerPlay === 'scissors ✌️' && cpuPlay === 'rock 👊') {
         result = 'cpu'
     } // gana cpu
 
-    else if (playerPlay === 'rock' && cpuPlay === 'scissors' ||
-    playerPlay === 'paper' && cpuPlay === 'rock' ||
-    playerPlay === 'scissors' && cpuPlay === 'paper') {
+    else if (playerPlay === 'rock 👊' && cpuPlay === 'scissors ✌️' ||
+    playerPlay === 'paper 🤚' && cpuPlay === 'rock 👊' ||
+    playerPlay === 'scissors ✌️' && cpuPlay === 'paper 🤚') {
         result = 'player'
     } // gana player
     return result
@@ -60,6 +89,15 @@ function generateFromZeroToTwo() { // genera un numero de 0 a 2
     return Math.floor(Math.random() * 3)
 }
 
+function isAPositiveNumber(input) { // regex para aceptar numeros enteros superiores a 0
+    var regex = /^[+]?\d*\.?\d+$/
+    if (regex.test(input)){
+        return true
+    } else {
+        return false
+    }
+}
+
 function isValidPlay(input) { // regex para aceptar solo r, p o s
     var regex = /^[rps]$/
     if (regex.test(input)){
@@ -69,4 +107,11 @@ function isValidPlay(input) { // regex para aceptar solo r, p o s
     }
 }
 
-playTurn()
+function restartGame() {
+    numberOfGames = 0
+    numberOfGamesPlayed = 0
+    gamesWonCpu = 0
+    gamesWonPlayer = 0
+}
+
+chooseNumberOfMatches()
