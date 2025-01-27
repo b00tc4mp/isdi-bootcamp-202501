2d1463b80410522a9a8dfa83365d13c7b1653fdb
manuelbarzi <manuelbarzi@gmail.com>
2025-01-27 14:59:26 +0100

    add guess-number and store base code #1

---
+++ staff/manuel-barzi/playground/guess-number/main.js
@@ -0,0 +1,12 @@
+/*
+player
+- adivinar numero random que sólo conoce la maquina (entre 0 y 100)
+- tiene 10 intentos
+- si intento (numero) tiene una diferencia >= 50 con el random, entonces "very cold"
+- si intento tiene una diferencia < 50 y >= 30, entonces "cold"
+- si intento tiene una diferencia < 30 y >= 20, entonces "tempered"
+- si intento tiene una diferencia < 20 y >= 10, entonces "warm"
+- si intento tiene una diferencia < 10 y >= 5, entonces "hot"
+- si intento tiene una diferencia < 5 y >= 1, entonces "very hot"
+- si acierta en menos de 10 intentos, win! por el contrario lost!
+*/
