7bcf5b247c21806685e653d27392639478e434a1
Marc Ramos i Jordà <ramos13@MacBook-Air-de-Marc.local>
2025-01-29 19:11:08 +0100

    create guessNumber main2 & update main #6

--- staff/marc-ramos/playground/guessNumber/main2.js
+++ staff/marc-ramos/playground/guessNumber/main2.js
@@ -13,32 +13,23 @@
 
 // DATA
 
-var randomNumber = 0
+var randomNumber = generateRandomNumber(0,100)
 var maxChances = 10
 var userChances = 0
 var playerNumber = 0
-var chancesLeft = 10
+var chancesLeft = maxChances - userChances
 var numberDiference = 0
-var winner = false
 
 // LOGIC
 
-function calculateChances(){
-    if(maxChances != 0)
-     chancesLeft--
-}
-
-function addRandomNumber(){
-    randomNumber = generateRandomNumber(0,100)
-}
-
 function generateRandomNumber(){
-    
-    return randomNumber =  Math.floor(Math.random() * (101))    
+    return randomNumber =  Math.floor(Math.random() * (100-0 + 1)) + 0     
 }
 
 function differenceNumbers(){
-    if (playerNumber > randomNumber){
+    if (playerNumber === randomNumber){
+        winGame()
+    } else if (playerNumber > randomNumber){
         numberDiference = playerNumber - randomNumber
     } else if (randomNumber > playerNumber){
         numberDiference = randomNumber - playerNumber
@@ -48,62 +39,50 @@ function differenceNumbers(){
 // PRESENTATION
 
 function askNumber(){
+    playerNumber = 0
     playerNumber = prompt ('What number is it? ' + '\n' + 'You have ' + chancesLeft + ' chances left')
 }
 
 function winGame(){
-    if (playerNumber === randomNumber){
-    winner = true
     alert ('Congratulations! YOU WIN! with ' + chancesLeft + ' chances left' + '\n' + 'The number was ' + randomNumber)
- 
-    } else {
-        maxChances--
-    }
 }
 
 function checkNumber(){
     if (numberDiference >= 50){
 
         alert ('Very cold!' + '\n' + 'You have ' + chancesLeft + ' chances left')
+        userChances++
+        askNumber()
 
     } else if (numberDiference < 50 && numberDiference >= 30){
 
         alert ('Cold!' + '\n' + 'You have ' + chancesLeft + ' chances left')
+        userChances++
+        askNumber()
 
     } else if (numberDiference < 30 && numberDiference >= 20){
 
         alert ('Tempered!' + '\n' + 'You have ' + chancesLeft + ' chances left')
+        userChances++
+        askNumber()
 
     } else if (numberDiference < 20 && numberDiference >= 10){
 
         alert ('Warm!' + '\n' + 'You have ' + chancesLeft + ' chances left')
+        userChances++
+        askNumber()
 
     } else if (numberDiference < 10 && numberDiference >= 5){
 
         alert ('Hot!' + '\n' + 'You have ' + chancesLeft + ' chances left')
+        userChances++
+        askNumber()
 
     } else if (numberDiference < 5 && numberDiference >= 1){
 
         alert ('Very hot!' + '\n' + 'You have ' + chancesLeft + ' chances left')
-
-    }
-}
-
-function gameAuto(){
-    addRandomNumber()
-
-    do {
+        userChances++
         askNumber()
-        differenceNumbers()
-        winGame()
-        calculateChances()
-        checkNumber()
-        
-    } while (winner === false && maxChances != 0) {
 
-        alert ('You lost!')
     }
 }
-
-gameAuto()
-
