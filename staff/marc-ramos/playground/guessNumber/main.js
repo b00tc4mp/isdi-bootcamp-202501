fd0df2e572cbce4238cadb524d7826f4be04f62a
Marc Ramos i Jordà <ramos13@MacBook-Air-de-Marc.local>
2025-01-30 17:27:47 +0100

    update guessNumber folder & files #6

--- staff/marc-ramos/playground/guessNumber/main.js
+++ staff/marc-ramos/playground/guessNumber/main.js
@@ -1,109 +1,100 @@
-/*
-- generar numero aleatorio entre 1 y 100
-- oportunidades maximas (10) y actuales del user
-- pedir numero al usuario (explicando que tiene que ser entre 1 y 100 y que tiene 10 oportunities)
-- si intento (numero) tiene una diferencia >= 50 con el random, entonces "very cold"
-- si intento tiene una diferencia < 50 y >= 30, entonces "cold"
-- si intento tiene una diferencia < 30 y >= 20, entonces "tempered"
-- si intento tiene una diferencia < 20 y >= 10, entonces "warm"
-- si intento tiene una diferencia < 10 y >= 5, entonces "hot"
-- si intento tiene una diferencia < 5 y >= 1, entonces "very hot"
-- si acierta en menos de 10 intentos, win! por el contrario lost!
-*/
+var data = {}
+var logic = {}
+var interface = {}
 
 // DATA
 
-var randomNumber = 0
-var maxChances = 10
-var userChances = 0
-var playerNumber = 0
-var chancesLeft = 10
-var numberDiference = 0
-var winner = false
+data.randomNumber = 0
+data.maxChances = 10
+data.userChances = 0
+data.playerNumber = 0
+data.chancesLeft = 10
+data.numberDiference = 0
+data.winner = false
 
 // LOGIC
 
-function calculateChances(){
-    if(maxChances != 0)
-     chancesLeft--
+logic.calculateChances = function (){
+    if(data.maxChances != 0)
+     data.chancesLeft--
 }
 
-function addRandomNumber(){
-    randomNumber = generateRandomNumber(0,100)
+logic.addRandomNumber = function (){
+    data.randomNumber = logic.generateRandomNumber(0,100)
 }
 
-function generateRandomNumber(){
+logic.generateRandomNumber = function (){
     
-    return randomNumber =  Math.floor(Math.random() * (101))    
+    return data.randomNumber =  Math.floor(Math.random() * (101))    
 }
 
-function differenceNumbers(){
-    if (playerNumber > randomNumber){
-        numberDiference = playerNumber - randomNumber
-    } else if (randomNumber > playerNumber){
-        numberDiference = randomNumber - playerNumber
+logic.differenceNumbers = function (){
+    if (data.playerNumber > data.randomNumber){
+        data.numberDiference = data.playerNumber - data.randomNumber
+    } else if (data.randomNumber > data.playerNumber){
+        data.numberDiference = data.randomNumber - data.playerNumber
+    }
+}
+
+logic.checkNumber = function (){
+    if (data.numberDiference >= 50){
+
+        alert ('Very cold!' + '\n' + 'You have ' + data.chancesLeft + ' chances left')
+
+    } else if (data.numberDiference < 50 && data.numberDiference >= 30){
+
+        alert ('Cold!' + '\n' + 'You have ' + data.chancesLeft + ' chances left')
+
+    } else if (data.numberDiference < 30 && data.numberDiference >= 20){
+
+        alert ('Tempered!' + '\n' + 'You have ' + data.chancesLeft + ' chances left')
+
+    } else if (data.numberDiference < 20 && data.numberDiference >= 10){
+
+        alert ('Warm!' + '\n' + 'You have ' + data.chancesLeft + ' chances left')
+
+    } else if (data.numberDiference < 10 && data.numberDiference >= 5){
+
+        alert ('Hot!' + '\n' + 'You have ' + data.chancesLeft + ' chances left')
+
+    } else if (data.numberDiference < 5 && data.numberDiference >= 1){
+
+        alert ('Very hot!' + '\n' + 'You have ' + data.chancesLeft + ' chances left')
+
+    }
+}
+
+logic.winGame = function (){
+    if (data.playerNumber === data.randomNumber){
+    data.winner = true
+    alert ('Congratulations! YOU WIN! with ' + data.chancesLeft + ' chances left' + '\n' + 'The number was ' + data.randomNumber)
+ 
+    } else {
+        data.maxChances--
     }
 }
 
 // PRESENTATION
 
-function askNumber(){
-    playerNumber = prompt ('What number is it? ' + '\n' + 'You have ' + chancesLeft + ' chances left')
+interface.askNumber = function (){
+    data.playerNumber = Number(prompt ('What number is it? ' + '\n' + 'You have ' + data.chancesLeft + ' chances left'))
 }
 
-function winGame(){
-    if (playerNumber === randomNumber){
-    winner = true
-    alert ('Congratulations! YOU WIN! with ' + chancesLeft + ' chances left' + '\n' + 'The number was ' + randomNumber)
- 
-    } else {
-        maxChances--
-    }
-}
-
-function checkNumber(){
-    if (numberDiference >= 50){
-
-        alert ('Very cold!' + '\n' + 'You have ' + chancesLeft + ' chances left')
-
-    } else if (numberDiference < 50 && numberDiference >= 30){
-
-        alert ('Cold!' + '\n' + 'You have ' + chancesLeft + ' chances left')
-
-    } else if (numberDiference < 30 && numberDiference >= 20){
-
-        alert ('Tempered!' + '\n' + 'You have ' + chancesLeft + ' chances left')
-
-    } else if (numberDiference < 20 && numberDiference >= 10){
-
-        alert ('Warm!' + '\n' + 'You have ' + chancesLeft + ' chances left')
-
-    } else if (numberDiference < 10 && numberDiference >= 5){
-
-        alert ('Hot!' + '\n' + 'You have ' + chancesLeft + ' chances left')
-
-    } else if (numberDiference < 5 && numberDiference >= 1){
-
-        alert ('Very hot!' + '\n' + 'You have ' + chancesLeft + ' chances left')
-
-    }
-}
-
-function gameAuto(){
-    addRandomNumber()
-
+interface.gameAuto = function (){
+    logic.addRandomNumber()
+    console.log(data.randomNumber)
     do {
-        askNumber()
-        differenceNumbers()
-        winGame()
-        calculateChances()
-        checkNumber()
+        interface.askNumber()
+        logic.differenceNumbers()
+        logic.winGame()
+        logic.calculateChances()
+        logic.checkNumber()
         
-    } while (winner === false && maxChances != 0) {
+    } while (data.winner === false && data.maxChances != 0) {
 
         alert ('You lost!')
     }
 }
 
-gameAuto()
+interface.gameAuto()
 
