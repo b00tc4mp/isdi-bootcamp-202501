71aa07b8d65d3e1dad8a31692739cb1921e5c947
Ans1011 <ammercade@gmail.com>
2025-02-02 20:50:58 +0100

    upgrade store version 1 #13

--- staff/anna-melian/playground/store/main2.js
+++ staff/anna-melian/playground/store/main2.js
@@ -46,6 +46,7 @@ data.strawberries = {
 }
 
 data.products = [data.apple, data.tomato, data.carrot, data.banana, data.lettuce, data.melon, data.strawberries]
+data.cart = []
 
 logic.helper = {
 
@@ -92,15 +93,46 @@ logic.helper = {
         if (!valid) {
             throw new Error('Invalid option')
         }
-    }
+    },
 
+    invalidNumber: function (number) {
+        var isANumber = false
+        if (typeof number === 'string') {
+            if (Number(number) <= 100 && Number(number) > 0) {
+                isANumber = true
+            }
+        }
+        if (!isANumber) {
+            throw new Error('Invalid option')
+
+        }
+    },
+
+    idExist: function (id) {
+        var productExist = false
+        for (var i = 0; i < data.products.length; i++) {
+            if (id === data.products[i].id) {
+                productExist = true
+            }
+        }
+        if (!productExist) {
+            throw new Error("This product doesn't exist")
+        }
+
+    },
+
+}
+
+logic.getProducts = function () {
+    return data.products
 }
 
 interface.showTheProducts = function () {
     try {
+        var products = logic.getProducts()
         var table = ''
-        for (var i = 0; i < data.products.length; i++) {
-            var product = data.products[i]
+        for (var i = 0; i < products.length; i++) {
+            var product = products[i]
             table += 'ID: ' + product.id + '  NAME: ' + product.name + '  TYPE: ' + product.type + '  PRICE: ' + product.price + '\n'
         }
         alert(table)
@@ -121,24 +153,11 @@ interface.askUser = function () {
 
 }
 
-logic.searchProduct = function (id) {
-    try {
-        logic.helper.invalidIdOption(id)
-        var productExist = false
-        for (var i = 0; i < data.products.length; i++) {
-            if (id === data.products[i].id) {
-                productExist = true
-            }
-        }
-    } catch (error) {
-        alert(error.message)
-    }
-
-}
 
 interface.showTheMatch = function (id) {
     try {
-        logic.helper.invalidIdOption(id)
+        logic.helper.invalidText(id)
+        logic.helper.idExist(id)
         var matchProduct = ''
         for (var i = 0; i < data.products.length; i++) {
             if (id === data.products[i].id) {
@@ -153,11 +172,11 @@ interface.showTheMatch = function (id) {
 
 }
 
-interface.askUserIdProduct = function () {
+interface.askUserIdProductSearch = function () {
     try {
         var id = prompt("What's the product id?")
-        logic.helper.invalidIdOption(id)
-        logic.searchProduct(id)
+        logic.helper.invalidText(id)
+        logic.helper.idExist(id)
         interface.showTheMatch(id)
 
     } catch (error) {
@@ -166,21 +185,78 @@ interface.askUserIdProduct = function () {
 }
 
 interface.askUserNewProduct = function () {
-    var generalNameProduct = prompt("What's the product general name ? ex. cookie, beef, chiken, onion ")
-    logic.helper.invalidText(generalNameProduct)
-    var nameProduct = prompt("What's the product name? ex. Oreo, burger, sausage ")
-    logic.helper.invalidText(nameProduct)
-    var typeProduct = prompt("What's the product type? ")
-    logic.helper.invalidText(typeProduct)
-    var priceProduct = prompt("What's the product price?")
-    logic.helper.invalidText(priceProduct)
+    try {
+        var generalNameProduct = prompt("What's the product general name ? ex. cookie, burger, sausage  ")
+        logic.helper.invalidText(generalNameProduct)
+        var nameProduct = prompt("What's the product name? ex. Oreo cookies, beef burger , chiken sausage ")
+        logic.helper.invalidText(nameProduct)
+        var typeProduct = prompt("What's the product type? ")
+        logic.helper.invalidText(typeProduct)
+        var priceProduct = prompt("What's the product price?")
+        logic.helper.invalidNumber(priceProduct)
+        logic.createNewObject(generalNameProduct, nameProduct, typeProduct, priceProduct)
+
+    } catch (error) {
+        alert(error.message)
+    }
+
+
 }
 
 logic.createNewObject = function (object, objectName, objectType, objectPrice) {
     var object = {
-        id: object[0] + object[1] + object[2] + '-' + objectName[0] + objectName[1] + object[2]
+        id: object[0] + object[1] + object[2] + '-' + objectName[0] + objectName[1] + object[2],
+        name: objectName,
+        type: objectType,
+        price: objectPrice,
     }
+    data.products[data.products.length] = object
 
+}
+
+interface.askUserIdProductAddCart = function () {
+    try {
+        var id = prompt("What's the product id you want to add to the cart?")
+        logic.helper.invalidText(id)
+        logic.helper.idExist(id)
+        logic.addProductToCart(id)
+
+
+    } catch (error) {
+        alert(error.message)
+    }
+}
+
+logic.addProductToCart = function (id) {
+    var selectProduct = ''
+    for (var i = 0; i < data.products.length; i++) {
+        if (id === data.products[i].id) {
+            data.cart[data.cart.length] = data.products[i]
+        }
+    }
+}
+
+logic.cartStatus = function () {
+    if (data.cart.length === 0) {
+        throw new Error('The cart is empty')
+    }
+    return data.cart
+}
+
+interface.showTheCart = function () {
+    try {
+        var cart = logic.cartStatus()
+        var table = ''
+        var totalPrice = 0
+        for (var i = 0; i < cart.length; i++) {
+            var product = cart[i]
+            table += 'ID: ' + product.id + '  NAME: ' + product.name + '  TYPE: ' + product.type + '  PRICE: ' + product.price + '\n'
+            totalPrice += Number(product.price)
+        }
+        alert(table + '\nTotal price: ' + totalPrice)
+    } catch (error) {
+        alert(error.message)
+    }
 
 }
 
@@ -190,17 +266,16 @@ logic.WhatsUserElection = function (num) {
     if (num == 0) {
         interface.showTheProducts()
     } else if (num == 1) {
-        interface.askUserIdProduct()
+        interface.askUserIdProductSearch()
     } else if (num == 2) {
-
+        interface.askUserNewProduct()
+    } else if (num == 3) {
+        interface.askUserIdProductAddCart()
+    } else if (num == 4) {
+        interface.showTheCart()
     }
 
 }
-
-
-
-
-
 
 
 console.clear()
