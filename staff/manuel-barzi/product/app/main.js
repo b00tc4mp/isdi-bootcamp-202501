d245964be965dc084d58b18932f0c4345b58f5c9
manuelbarzi <manuelbarzi@gmail.com>
2025-02-04 17:47:00 +0100

    implement landing nodes tree and start register #40

--- staff/manuel-barzi/product/app/main.js
+++ staff/manuel-barzi/product/app/main.js
@@ -13,11 +13,43 @@ landing.appendChild(landingLogo)
 var landingLogoText = document.createTextNode('Logo')
 landingLogo.appendChild(landingLogoText)
 
+var landingRegisterAnchor = document.createElement('a')
+landing.appendChild(landingRegisterAnchor)
+
+var landingRegisterAnchorText = document.createTextNode('Register')
+landingRegisterAnchor.appendChild(landingRegisterAnchorText)
+
+var landingOrText = document.createTextNode('or')
+landing.appendChild(landingOrText)
+
+var landingLoginAnchor = document.createElement('a')
+landing.appendChild(landingLoginAnchor)
+
+var landingLoginAnchorText = document.createTextNode('Login')
+landingLoginAnchor.appendChild(landingLoginAnchorText)
+
 
 // register
 
 var register = document.createElement('div')
 document.body.appendChild(register)
+
+var registerLogo = document.createElement('h1')
+register.appendChild(registerLogo)
+
+var registerLogoText = document.createTextNode('Logo')
+registerLogo.appendChild(registerLogoText)
+
+var registerForm = document.createElement('form')
+register.appendChild(registerForm)
+
+var registerFormNameLabel = document.createElement('label')
+registerForm.appendChild(registerFormNameLabel)
+
+var registerFormNameLabelText = document.createTextNode('Name')
+registerFormNameLabel.appendChild(registerFormNameLabelText)
+
+// ...
 
 // login
 
