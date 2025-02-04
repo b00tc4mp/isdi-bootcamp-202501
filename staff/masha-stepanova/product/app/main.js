56c1c390ad9aee9ee5aa2359ad8b5d1de8544820
Masha-Ste <mariiastepanova2000@gmail.com>
2025-02-04 18:40:53 +0100

    create html document with js in app - main.js #43

---
+++ staff/masha-stepanova/product/app/main.js
@@ -0,0 +1,157 @@
+console.clear()
+
+// landing
+
+var landing = document.createElement('div')
+document.body.appendChild(landing)
+
+var landingLogo = document.createElement('h1')
+landing.appendChild(landingLogo)
+
+var landingLogoText = document.createTextNode('Logo')
+landingLogo.appendChild(landingLogoText)
+
+
+var landingRegisterAnchor = document.createElement('a')
+landing.appendChild(landingRegisterAnchor)
+
+var landingRegisterAnchorText = document.createTextNode('Register')
+landingRegisterAnchor.appendChild(landingRegisterAnchorText)
+
+var landingOrText = document.createTextNode(' or ')
+landingRegisterAnchor.appendChild(landingOrText)
+
+var landingLoginAnchor = document.createElement('a')
+landing.appendChild(landingLoginAnchor)
+
+var landingLoginAnchorText = document.createTextNode('Login')
+landingRegisterAnchor.appendChild(landingLoginAnchorText)
+
+// Register
+
+var register = document.createElement('div')
+document.body.appendChild(register)
+
+var registerLogo = document.createElement('h1')
+register.appendChild(registerLogo)
+
+var registerLogoText = document.createTextNode('Logo')
+registerLogo.appendChild(registerLogoText)
+
+var registerForm = document.createElement('form')
+register.appendChild(registerForm)
+
+var registerFormNameLabel = document.createElement('label')
+registerForm.appendChild(registerFormNameLabel)
+
+var registerFormNameLabelText = document.createTextNode('Name ')
+registerFormNameLabel.appendChild(registerFormNameLabelText)
+
+var registerFormNameInput = document.createElement('input')
+registerForm.appendChild(registerFormNameInput)
+
+var registerFormEmailLabel = document.createElement('label')
+registerForm.appendChild(registerFormEmailLabel)
+
+var registerFormEmailLabelText = document.createTextNode(' E-mail ')
+registerFormEmailLabel.appendChild(registerFormEmailLabelText)
+
+var registerFormEmailInput = document.createElement('input')
+registerForm.appendChild(registerFormEmailInput)
+
+var registerFormUsernameLabel = document.createElement('label')
+registerForm.appendChild(registerFormUsernameLabel)
+
+var registerFormUsernameLabelText = document.createTextNode(' Username ')
+registerFormUsernameLabel.appendChild(registerFormUsernameLabelText)
+
+var registerFormUsernameInput = document.createElement('input')
+registerForm.appendChild(registerFormUsernameInput)
+
+var registerFormPasswordLabel = document.createElement('label')
+registerForm.appendChild(registerFormPasswordLabel)
+
+var registerFormPasswordLabelText = document.createTextNode(' Password ')
+registerFormPasswordLabel.appendChild(registerFormPasswordLabelText)
+
+var registerFormPasswordInput = document.createElement('input')
+registerForm.appendChild(registerFormPasswordInput)
+
+var registerFormRegisterButton = document.createElement('button')
+registerForm.appendChild(registerFormRegisterButton)
+
+// Login
+
+var login = document.createElement('div')
+document.body.appendChild(login)
+
+var loginLogo = document.createElement('h1')
+login.appendChild(loginLogo)
+
+var loginLogoText = document.createTextNode('Logo')
+loginLogo.appendChild(loginLogoText)
+
+var loginForm = document.createElement('form')
+login.appendChild(loginForm)
+
+var loginFormUsernameLabel = document.createElement('label')
+loginForm.appendChild(loginFormUsernameLabel)
+
+var loginFormUsernameLabelText = document.createTextNode('Username ')
+loginFormUsernameLabel.appendChild(loginFormUsernameLabelText)
+
+var loginFormUsernameInput = document.createElement('input')
+loginForm.appendChild(loginFormUsernameInput)
+
+var loginFormPasswordLabel = document.createElement('label')
+loginForm.appendChild(loginFormPasswordLabel)
+
+var loginFormPasswordLabelText = document.createTextNode(' Pasword ')
+loginFormPasswordLabel.appendChild(loginFormPasswordLabelText)
+
+var loginFormPasswordInput = document.createElement('input')
+loginForm.appendChild(loginFormPasswordInput)
+
+var loginFormLoginButton = document.createElement('button')
+loginForm.appendChild(loginFormLoginButton)
+
+// home
+
+var home = document.createElement('div')
+document.body.appendChild(home)
+
+var homeLogo = document.createElement('h1')
+home.appendChild(homeLogo)
+
+var homeLogoText = document.createTextNode('Logo')
+homeLogo.appendChild(homeLogoText)
+
+var homePostArticle = document.createElement('article')
+homeLogo.appendChild(homePostArticle)
+
+var homePostArticleUsername = document.createElement('h10')
+homePostArticle.appendChild(homePostArticleUsername)
+
+var homePostArticleUsernameText = document.createTextNode('username')
+homePostArticleUsername.appendChild(homePostArticleUsernameText)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Login
+
+// element.style.backgroundColor = 'color'
+// 'article', elemento tipo post
