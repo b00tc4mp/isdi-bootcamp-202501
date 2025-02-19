38a4762c751f3b2809b611a066ad443e556568aa
JorgeLara0322 <jorge.lara2203g@gmail.com>
2025-02-20 00:47:14 +0100

    add calculator project and reorganized files into new folders(games,manuals) #10

---
+++ staff/jorge-lara/playground/calculator/main.js
@@ -0,0 +1,41 @@
+const title = React.createElement('h1', { children: 'Hello, Calculator! 🧮' })
+
+const number1Label = React.createElement('label', { children: 'Number 1' })
+const number1Input = React.createElement('input', { type: 'number', name: 'number1' })
+
+const number2Label = React.createElement('label', { children: 'Number 2' })
+const number2Input = React.createElement('input', { type: 'number', name: 'number2' })
+
+const submitButton = React.createElement('button', {
+    type: 'submit',
+    children: '='
+})
+
+const form = React.createElement('form', {
+    children: [
+        number1Label,
+        number1Input,
+        number2Label,
+        number2Input,
+        submitButton
+    ],
+    onSubmit(event) {
+        event.preventDefault()
+
+        const domForm = event.target
+
+        const domNumber1Input = domForm.querySelector('input[name=number1]')
+        const domNumber2Input = domForm.querySelector('input[name=number2]')
+
+        const number1 = domNumber1Input.value
+        const number2 = domNumber2Input.value
+
+        console.log('hello submit', number1, number2)
+    }
+})
+
+const resultParagraph = React.createElement('p')
+
+const rootElement = document.querySelector('div#root')
+const root = ReactDOM.createRoot(rootElement)
+root.render([title, form, resultParagraph])
