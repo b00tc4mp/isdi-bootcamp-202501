95b3f8f39fa8054815e705e714e8999980cd176a
Aarchyy89 <aaronbmaartin@gmail.com>
2025-02-19 15:40:05 +0100

    react test play #2

--- staff/aaron-barrios/playground/arrays-remake/splice.js
+++ staff/aaron-barrios/playground/arrays-remake/splice.js
@@ -1,6 +1,9 @@
 Array.prototype.splice = function (indexFrom, removeCount, ...elements) {
     const removedElements = []
 
+    //DEFENSIVE PROGRAMMING
+
+
     for (let i = indexFrom; i < indexFrom + removeCount; i++) {
         const currentElement = this[i]
 
@@ -9,3 +12,28 @@ Array.prototype.splice = function (indexFrom, removeCount, ...elements) {
         this[i] = elements[i - indexFrom]
     }
 }
+
+var array = ["ant", "bison", "camel", "duck", "elephant"]
+var b = function (indexFrom, deleteCount, ...elements) {
+    const removedElements = []
+
+    //DEFENSIVE PROGRAMMING
+    if (indexFrom >= array.length || arguments.length === 0) {
+        return removedElements
+    }
+
+    // --- CASE 1 ARGUMENT ----
+    if (arguments.length === 1) {
+        if (fromIndex >= 0) {
+            for (let i = fromIndex; i < fromIndex + deleteCount; i++) {
+                const currentElement = array[i]
+
+                removedElements.push(currentElement)
+
+                array[i] = elements[i - indexFrom]
+            }
+        }
+    }
+}
+// b(3)
+array.splice(0)
---
+++ staff/aaron-barrios/playground/react-test/main.js
@@ -0,0 +1,43 @@
+//ELEMENTOS INMUTABLES
+const title = React.createElement('h1', { children: 'Hello, Calculator! 🧮' });
+
+const number1Label = React.createElement('label', { children: 'Number 1' });
+const number1Input = React.createElement('input', { type: 'number', name: 'number1' });
+
+const number2Label = React.createElement('label', { children: 'Number 2' });
+const number2Input = React.createElement('input', { type: 'number', name: 'number2' });
+
+let resultParagraph = React.createElement('p', { children: 'Result: ' });
+
+const submitButton = React.createElement('button', { type: 'submit', children: '=' });
+
+const form = React.createElement('form', {
+    children: [
+        number1Label,
+        number1Input,
+        number2Label,
+        number2Input,
+        submitButton
+    ],
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        const domForm = event.target;
+        const domNumber1Input = domForm.querySelector('input[name=number1]')
+        const domNumber2Input = domForm.querySelector('input[name=number2]')
+
+        const number1 = Number(domNumber1Input.value)
+        const number2 = Number(domNumber2Input.value)
+
+        const result = number1 + number2
+
+        resultParagraph = React.createElement('p', { children: `Result: ${result}` })
+
+        root.render([title, form, resultParagraph]);
+    }
+});
+
+const rootElement = document.querySelector('div#root');
+const root = ReactDOM.createRoot(rootElement);
+root.render([title, form, resultParagraph]);
