31acb7e5d7b347c856283ac1a234f55ccc443383
JorgeLara0322 <jorge.lara2203g@gmail.com>
2025-02-19 01:18:50 +0100

    added arrays remakes forEach,includes and pop WIP #10

---
+++ staff/jorge-lara/playground/Arrays/arrays-remake/forEach.js
@@ -0,0 +1,7 @@
+Array.prototype.forEach = function (callback, context){
+    for (let i = 0; i < this.length; i++) {
+        const element = this[i];
+
+        callback.call(context, element, i , this);
+    }
+}
