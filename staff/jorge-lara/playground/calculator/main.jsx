05d013e3afc92a1198e9cb6dd5043b0fa4f21f58
JorgeLara0322 <jorge.lara2203g@gmail.com>
2025-02-20 18:59:30 +0100

    moving from js to jsx the simple calculator #10

---
+++ staff/jorge-lara/playground/calculator/main.jsx
@@ -0,0 +1,43 @@
+const rootElement = document.querySelector('div#root');
+const root = ReactDOM.createRoot(rootElement);
+
+const useState = React.useState;
+
+function Calculator(){
+    const [result, setResult] = useState(0);
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+
+        const domForm = event.target;
+
+        const domNumber1Input = domForm.querySelector('input[name=number1');
+        const domNumber2Input = domForm.querySelector('input[name=number2');
+
+        const number1 = Number(domNumber1Input.value);
+        const number2 = Number(domNumber2Input.value);
+
+        const result = number1 + number2;
+
+        console.log('hello submit', result);
+
+        setResult(result);
+    }
+
+    return <>
+        <h1>Hello, World!</h1>
+
+        <form onSubmit={handleSubmit}>
+            <label>Number1</label>
+            <input type="number" name="number1"/>
+            +
+            <label>Number2</label>
+            <input type="number" name="number2"/>
+            <button type="submit">=</button>
+        </form>
+
+        <p>{result}</p>
+    </>
+}
+
+root.render(<Calculator/>);
