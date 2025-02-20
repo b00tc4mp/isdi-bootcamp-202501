0d6eb3f0c717462cd6a52dbc918ed7d904bb97c1
Ans1011 <ammercade@gmail.com>
2025-02-20 10:24:20 +0100

    calculator version 0 #13

---
+++ staff/anna-melian/playground/calculator/main.js
@@ -0,0 +1,50 @@
+const title = React.createElement('h1', { children: 'Hello,Calculator!' })
+
+const number1Label = React.createElement('label', { children: 'Number 1' })
+const number1Input = React.createElement('input', { type: 'number', name: 'number1' })
+
+const number2Label = React.createElement('label', { children: 'Number 2' })
+const number2Input = React.createElement('input', { type: 'number', name: 'number2' })
+
+const submitButton = React.createElement('button', {
+    type: 'submit',
+    children: '='
+})
+
+const form = React.createElement('form', {
+    children: [
+        number1Label,
+        number1Input,
+        '+',
+        number2Label,
+        number2Input,
+        submitButton
+    ],
+    onSubmit(event) {
+        event.preventDefault()
+
+        const domForm = event.target
+
+        const domNumber1Input = domForm.querySelector('input[name=number1]')
+        const domNumber2Input = domForm.querySelector('input[name=number2]')
+
+        const number1 = Number(domNumber1Input.value)
+        const number2 = Number(domNumber2Input.value)
+
+        const result = number1 + number2
+
+        console.log('result:' + result)
+
+        const resultParagraph = React.createElement('p', {
+            children: result
+        })
+
+        root.render([title, form, resultParagraph])
+    }
+})
+const resultParagraph = React.createElement('p')
+
+
+const rootElement = document.querySelector('div#root')
+const root = ReactDOM.createRoot(rootElement)
+root.render([title, form, resultParagraph])
