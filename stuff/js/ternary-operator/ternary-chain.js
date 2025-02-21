faaeb90a1bbd7f9cd8f21ff34958c5c6f2646cf9
manuelbarzi <manuelbarzi@gmail.com>
2025-02-21 10:14:03 +0100

    add dynamic-property-access and ternary-operator-chain demos #37

---
+++ stuff/js/ternary-operator/ternary-chain.js
@@ -0,0 +1,5 @@
+var v = !true, w = false, x = !true, y = true, z = false
+
+var salute = v ? 'ola' : w ? 'ciao' : x ? 'pryvit' : y ? 'hello' : z ? 'hola' : 'hallo'
+console.log(salute)
+// VM502:4 hello
