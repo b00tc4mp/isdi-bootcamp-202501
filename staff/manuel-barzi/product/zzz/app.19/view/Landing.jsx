09ef8b78d91df935582cea95fe0e773a453ffc2d
manuelbarzi <manuelbarzi@gmail.com>
2025-02-21 13:07:22 +0100

    implement navigation and submits (register and login) in app with react #40

--- staff/manuel-barzi/product/zzz/app.19/view/Landing.jsx
+++ staff/manuel-barzi/product/zzz/app.19/view/Landing.jsx
@@ -1,6 +1,4 @@
 function Landing({ onRegisterClick, onLoginClick }) {
-    console.debug('Landing -> render')
-
     return <div>
         <h1>Logo</h1>
         <a onClick={onRegisterClick}>Register</a>or<a onClick={onLoginClick}>Login</a>
