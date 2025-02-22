f6e76b982e9d263f7083b3e19b6a0ee201b8db70
lucianoparavanit <lucianoparavanit@gmail.com>
2025-02-22 18:32:08 +0100

    add react to product #47

---
+++ staff/luciano-paravan/product/app/main.jsx
@@ -0,0 +1,6 @@
+console.log('Hello App!')
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+//root.render(<Landing/>)
+root.render(<Login/>)
---
+++ staff/luciano-paravan/product/app/view/Login.jsx
@@ -0,0 +1,16 @@
+function Login () {
+    return <>
+        <div>
+            <h1>Logo</h1>
+            <h3>Login</h3>
+            <form>
+                <label></label>
+                <input type="text" />
+                <label></label>
+                <input type="password" />
+                <button type="submit">Login</button>
+            </form>
+            <a>Register</a>
+        </div>
+    </>
+}
