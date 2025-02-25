cb1d1234a3b826d6fd13100bcd96e60a85f4429f
Sergi <sergicpiqueras@gmail.com>
2025-02-25 18:35:25 +0100

    update methods; add find method #5

--- staff/sergi-castellar/playground/array-methods-redone/reduce-tested/reduce.test.js
+++ staff/sergi-castellar/playground/array-methods-redone/reduce-tested/reduce.test.js
@@ -52,23 +52,3 @@ console.info('CASE find the highest number')
 
     console.assert(result === 9, 'result === 9')
 }
-
-console.info('CASE ')
-
-{
-    const numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5]
-    const result = numbers.reduce((acumulador, num) => num > acumulador ? num : acumulador, -Infinity)
-
-    console.assert(numbers.length === 9, 'numbers.length === 9')
-    console.assert(numbers[0] === 3, 'numbers[0] === 3')
-    console.assert(numbers[1] === 1, 'numbers[1] === 1')
-    console.assert(numbers[2] === 4, 'numbers[2] === 4')
-    console.assert(numbers[3] === 1, 'numbers[3] === 1')
-    console.assert(numbers[4] === 5, 'numbers[4] === 5')
-    console.assert(numbers[5] === 9, 'numbers[5] === 9')
-    console.assert(numbers[6] === 2, 'numbers[6] === 2')
-    console.assert(numbers[7] === 6, 'numbers[7] === 6')
-    console.assert(numbers[8] === 5, 'numbers[8] === 5')
-
-    console.assert(result === 9, 'result === 9')
-}
