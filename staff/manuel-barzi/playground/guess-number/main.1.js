63300c65bfca511040b5f40165268c71c3c559c3
manuelbarzi <manuelbarzi@gmail.com>
2025-02-26 10:58:19 +0100

    move interface operations to input and output in guess-number #1

--- staff/manuel-barzi/playground/guess-number/main.1.js
+++ staff/manuel-barzi/playground/guess-number/main.1.js
@@ -109,51 +109,47 @@ const logic = {
 }
 
 const interface = {
-    input: {
-        start() {
-            try {
-                logic.initializeNumberToGuess()
-            } catch (error) {
-                console.error(error)
+    start() {
+        try {
+            logic.initializeNumberToGuess()
+        } catch (error) {
+            console.error(error)
 
-                alert(error.message)
-            }
-        },
-
-        askNumber() {
-            try {
-                const number = Number(prompt('Number?'))
-
-                logic.tryNumber(number)
-            } catch (error) {
-                console.error(error)
-
-                alert(error.message)
-            }
-        },
-
-        restart() {
-            try {
-                logic.reset()
-            } catch (error) {
-                console.error(error)
-
-                alert(error.message)
-            }
+            alert(error.message)
         }
     },
-    output: {
-        viewStatus() {
-            try {
-                const { attempts, temperature, attemptedNumbers, won, lost, gameOver } = logic.getStatus()
 
-                alert(`temperate ${temperature}, attempts ${attempts}, attemptedNumbers ${attemptedNumbers}, won ${won}, lost ${lost}, gameOver ${gameOver}`)
-            } catch (error) {
-                console.error(error)
+    askNumber() {
+        try {
+            const number = Number(prompt('Number?'))
 
-                alert(error.message)
-            }
+            logic.tryNumber(number)
+        } catch (error) {
+            console.error(error)
+
+            alert(error.message)
+        }
+    },
+
+    viewStatus() {
+        try {
+            const { attempts, temperature, attemptedNumbers, won, lost, gameOver } = logic.getStatus()
+
+            alert(`temperate ${temperature}, attempts ${attempts}, attemptedNumbers ${attemptedNumbers}, won ${won}, lost ${lost}, gameOver ${gameOver}`)
+        } catch (error) {
+            console.error(error)
+
+            alert(error.message)
+        }
+    },
+
+    restart() {
+        try {
+            logic.reset()
+        } catch (error) {
+            console.error(error)
+
+            alert(error.message)
         }
     }
-
 }
