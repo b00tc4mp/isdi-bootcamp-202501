ac3707192757bcfa7e8e8ea4ba0243f757451f2e
Eugeni <ecastellsricart@gmail.com>
2025-02-28 15:29:58 +0100

    added app copy

---
+++ staff/eugeni-castells/product/app.2/App.jsx
@@ -0,0 +1,44 @@
+data.users = JSON.parse(localStorage.getItem("users"));
+data.posts = JSON.parse(localStorage.getItem("posts"));
+
+function App() {
+  const { useState } = React;
+
+  const [view, setView] = useState("landing");
+
+  const handleRegisterClick = () => {
+    setView("register");
+  };
+
+  const handleLoginClick = () => {
+    setView("login");
+  };
+
+  const handleHomeClick = () => {
+    setView("home");
+  };
+
+  return (
+    <>
+      {view === "landing" && (
+        <Landing
+          onRegisterClick={handleRegisterClick}
+          onLoginClick={handleLoginClick}
+        />
+      )}
+      {view === "register" && (
+        <Register
+          onLoginClick={handleLoginClick}
+          onRegisterSuccess={handleLoginClick}
+        />
+      )}
+      {view === "login" && (
+        <Login
+          onRegisterClick={handleRegisterClick}
+          onLoginSuccess={handleHomeClick}
+        />
+      )}
+      {view === "home" && <Home onLogoutClick={handleLoginClick} />}
+    </>
+  );
+}
