ade63aa52428941a0bc51d622023232591f54d81
BertaAsensio <bertagonzalez224@gmail.com>
2025-03-02 19:31:16 +0100

    upload r-p-s WIP #4

--- staff/berta-asensio/playground/rock-paper-scissors/main.js
+++ staff/berta-asensio/playground/rock-paper-scissors/main.js
@@ -6,10 +6,6 @@ const data = {
     
     counterPc: 0,
     counterPlayer: 0,
-
-    roundTie,
-    roundWin,
-    roundLost
 }
 
 const logic = {
@@ -22,19 +18,23 @@ const logic = {
         console.log(data.pcChoose)
     },
 
-    playerOptions(data.playerChoose) {
+    playerOptions(playerChoose) {
 
-        if(data.posibleOptions.includes(data.playerChoose)) { 
+        if(data.posibleOptions.includes(playerChoose)) {
+            data.playerChoose = playerChoose
                 
             logic.validateRound()
 
-        } else { throw new TypeError ('invalid option type') }
-        },
+        } else { throw new Error ('invalid option') }
+    },
     
     validateRound() {
+
+        let result = ''
     
         if (data.pcChoose === data.playerChoose) {
-           //alert('Round tie! The two players add one point. \nYou have chosen ' + playerChoose + ', and your opponent ' + pcChoose + '.')
+            //alert('Round tie! The two players add one point. \nYou have chosen ' + playerChoose + ', and your opponent ' + pcChoose + '.')
+            result = 'tie'
             data.counterPlayer++
             data.counterPc++
             
@@ -42,24 +42,47 @@ const logic = {
             (data.pcChoose === data.posibleOptions[1] && data.playerChoose === data.posibleOptions[0]) ||
             (data.pcChoose === data.posibleOptions[2] && data.playerChoose === data.posibleOptions[1])) {
            // alert ('You lost the round! \nYou have chosen ' + playerChoose + ' and your opponent ' + pcChoose +'.')
-            data.counterPc++
+           result = 'lost' 
+           data.counterPc++
             
             
         } else if ((data.pcChoose === data.posibleOptions[0] && data.playerChoose === data.posibleOptions[1]) ||
             (data.pcChoose === data.posibleOptions[1] && data.playerChoose === data.posibleOptions[2]) ||
             (data.pcChoose === data.posibleOptions[2] && data.playerChoose === data.posibleOptions[0])) {
             //alert ('You won the round! \nYou have chosen ' + playerChoose + ' and your opponent '  + pcChoose + '.')
+            result = 'win'
             data.counterPlayer++
         
         } else {
+            throw new Error('Unexpected error ocurred')
+        }
+
+        return result
+        
+    },
+
+    getStatus() {
+
+        let finalResult = ''
+
+        if (data.counterPc === 3 && data.counterPlayer < 3) {
+            finalResult = 'LOST'
+        } else if (data.counterPlayer === 3 && data.counterPc < 3) {
+            finalResult = 'WIN'
+        } else if (data.counterPlayer === 3 && data.counterPc === 3) {
+            finalResult = 'TIE'
+        } else {
             throw new Error(error.message)
         }
-        console.log('Bookmark: Player - ' + data.counterPlayer + '| Opponent - ' + data.counterPc + '.')
-        //winGame()
-        //pcOptions()
-        //playerOptions()
-        
-        
+
+        return finalResult
+    },
+
+    resetGame() {
+        data.counterPc = 0
+        data.counterPlayer = 0
+        data.pcChoose = ''
+        data.playerChoose = ''        
     }
 }
 
@@ -68,6 +91,7 @@ const interface = {
     start () {
         try {
             logic.pcOptions()
+            this.askOptions()
         } catch (error) {
     
             console.error(error)
@@ -77,66 +101,50 @@ const interface = {
     
     askOptions() {
         try {
-            playerChoose = prompt('Please, Insert an option: rock, paper or scissors')
+            let playerChoose = prompt('Please, Insert an option: rock, paper or scissors')
             data.playerChoose = playerChoose.toLowerCase()
-            logic.playerOptions(data.playerChoose)
+           
+            let result = logic.playerOptions(data.playerChoose)
+            console.log(`Player chose: ${data.playerChoose}`)
+
+            if (result === 'tie') {
+                alert('Round tie! The two players add one point. \nYou have chosen ' + data.playerChoose + ', and your opponent ' + data.pcChoose + '.')
+            
+            } else if (result === 'lost') {
+                alert('You lost the round! \nYou have chosen ' + data.playerChoose + ' and your opponent ' + data.pcChoose +'.')
+            
+            } else if (result === 'win') {
+                alert('You won the round! \nYou have chosen ' + data.playerChoose + ' and your opponent '  + data.pcChoose + '.')
+            }
+
+            alert('Bookmark: Player - ' + data.counterPlayer + '| Opponent - ' + data.counterPc + '.')
+            this.setStatus()
                
         } catch (error) {
             console.error(error)
             alert('Wrong word. Try again and write well.')
-            logic.playerOptions()   
+            this.askOptions() 
         }
     },
-    
-    playRounds() {
+
+    setStatus() {
+
         try {
-            validateRound()
-    
-        } catch(error) {
-            console.error(error)
-            alert(error.message)
-        }
-    },
-    
-    viewStatus() {
-        try {
+            let finalResult = logic.getStatus()
+
+            if (finalResult === 'LOST') {
+                alert ('You lost the game! \njBookmark: Player - ' + data.counterPlayer + ' | Opponent - ' + data.counterPc + '.')  
+            } else if (finalResult === 'WIN') {
+                alert('Congratulations! You won the game! \nBookmark: Player - ' + data.counterPlayer + ' | Opponent - ' + data.counterPc + '.')
+            } else if (finalResult === 'TIE') {
+                alert('You have tied the game. Play again to tiebreaker')
+            }
+
+            logic.resetGame()
             
         } catch (error) {
             console.error(error)
-            
-            alert(error.message)            
+            alert(error.message)    
         }
-    },
-
-
-
-    function winGame() {
-        
-        if (counterPc === 3 && counterPlayer < 3) {
-            alert('You lost the game! \njBookmark: Player - ' + counterPlayer + ' | Opponent - ' + counterPc + '.')
-            return resetGame()
-            
-        } else if (counterPlayer === 3 && counterPc < 3) {
-            alert('Congratulations! You won the game! \nBookmark: Player - ' + counterPlayer + ' | Opponent - ' + counterPc + '.')
-            return resetGame()
-            
-        } else if ( (counterPlayer === 3 && counterPc === 3)) {
-            alert('You have tied the game. Play again to tiebreaker')
-            return resetGame()       
-        }
-        
-        pcOptions()
-        playerOptions()
-    }        
-    
-    
-            
-    function resetGame() {
-        counterPc = 0
-        counterPlayer = 0
-        pcOptions()
-        playerOptions()
-        
     }
-
 }
