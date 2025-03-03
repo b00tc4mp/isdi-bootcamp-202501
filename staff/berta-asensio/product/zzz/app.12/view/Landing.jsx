122a4fd65392bcd98abb7c9d6841f939f6efc441
BertaAsensio <bertagonzalez224@gmail.com>
2025-03-03 19:39:32 +0100

    add modified app.13 WIP #45

--- staff/berta-asensio/product/zzz/app.12/view/Landing.jsx
+++ staff/berta-asensio/product/zzz/app.12/view/Landing.jsx
@@ -1,8 +1,9 @@
+
 function Landing({onRegisterClick, onLoginClick}) {
     return <div className="landing-container">
         <div className="logo-container">
-        <img src="images/images.png" alt="Bee logo" className="bee-logo" />
-        <h1>Bee you</h1>
+            <img src="doc\images\logo-bee-you.png" alt="Bee logo" className="bee-logo" />
+            <h1>Bee you</h1>
         </div>
         <div className="landing-buttons">
             <button onClick={onRegisterClick}>Register</button>
