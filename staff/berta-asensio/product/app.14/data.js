6818321ec62b5f1ff3e15031f3653f511cf0e76e
BertaAsensio <bertagonzalez224@gmail.com>
2025-03-05 18:31:55 +0100

    add app v14 data separation in setters and getters #45

---
+++ staff/berta-asensio/product/app.14/data.js
@@ -0,0 +1,44 @@
+const data = {
+
+    uuid() {
+        return (Date.now() + Math.random()).toString(36).replace('.', '') 
+    },
+
+    get users() {
+        const users = JSON.parse(localStorage.users || '[]')
+
+        return users
+    },
+
+    set users(users) {
+        const json = JSON.stringify(users)
+
+        localStorage.users = json
+    },
+
+    
+    get userId() {
+        const id = JSON.parse(sessionStorage.userId || 'null')
+
+        return id
+    },
+
+    set userId(id) {
+        const json = JSON.stringify(id)
+
+        sessionStorage.userId = json
+    },
+
+    get posts() {
+        const posts = JSON.parse(localStorage.posts || '[]')
+
+        return posts
+    },
+
+    set posts(posts) {
+        const json = JSON.stringify(posts)
+
+        localStorage.posts = json
+
+    }
+}
