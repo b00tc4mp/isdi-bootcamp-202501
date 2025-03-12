421a85964ec42f110e208480d196487e030660e3
manuelbarzi <manuelbarzi@gmail.com>
2025-03-12 13:09:17 +0100

    staff/manuel-barzi/product/

---
+++ staff/manuel-barzi/product/api/logic/deletePost.test.js
@@ -0,0 +1,7 @@
+import { deletePost } from './deletePost.js'
+
+try {
+    deletePost('m7ypqlzdwy', 'm85tz8eq0j')
+} catch (error) {
+    console.error(error)
+}
---
+++ staff/manuel-barzi/product/api/logic/toggleLikePost.test.js
@@ -0,0 +1,7 @@
+import { toggleLikePost } from './toggleLikePost.js'
+
+try {
+    toggleLikePost('m7ypqlzdwy', 'm7yp3nf12ep')
+} catch (error) {
+    console.error(error)
+}
