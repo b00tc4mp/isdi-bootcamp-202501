c7d094f959c4749c2bcb0dd98f369273627355ae
dalle-31 <dallen-31@hotmail.com>
2025-03-12 13:19:00 +0100

    First tests with servers, knowing their methods #14

---
+++ staff/david-alonso/playground/five-server/logic/getFiles.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+const getFiles = () => {
+    const json = fs.readFileSync('data/files.json', 'utf-8')
+
+    const files = JSON.parse(json)
+
+    return files
+}
+
+module.exports = getFiles
