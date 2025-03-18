114a3e35c74b61d4b90da3e5f610b27a9b10bdeb
BertaAsensio <bertagonzalez224@gmail.com>
2025-03-18 17:59:26 +0100

    update changes to simple api rest #4

--- staff/berta-asensio/playground/express-restapi-crud/server.js
+++ staff/berta-asensio/playground/express-restapi-crud/server.js
@@ -2,29 +2,79 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
+let products = [
+    {
+        id: 1,
+        name: 'laptop',
+        price: 3000
+    }
+] //este arreglo lo creamos porque no vamos a crear una base de datos, por lo que almacenaremos datos aqui
 
 app.use(morgan('dev'))
+app.use(express.json()) //esto seria nuestro bodyParser, un middleware que me ayudará a parsear el body que yo mande al servidor. No hace falta ponerlo en la "función"
 
 //Empezamos a crear rutas
 
+//Ruta 1
 app.get('/products', (req, res) => {
-    res.send('obteniendo productos')
+    res.json(products)
 })
 
+//Ruta 2
 app.post('/products', (req, res) => {
-    res.send('creando productos')
+    const newProduct = {...req.body, id: products.length + 1}
+    products.push(newProduct)
+    res.send(newProduct)
 })
 
-app.put('/products', (req, res) => {
-    res.send('actualizando productos')
+//Ruta 3
+app.put('/products/:id', (req, res) => {
+
+    const newData = req.body
+    const productFound = products.find(function (product) {
+        //return product.id == req.params.id 
+        return product.id === parseInt(req.params.id)
+    })
+
+    if(!productFound) 
+        return res.status(404).json({
+            message: "Product not found" 
+        })
+    
+    products = products.map(p => p.id === parseInt(req.params.id) ? {...p, ...newData} : p)
+    
+    res.json({
+        message: "Product updated succesfully"
+    })
 })
 
-app.delete('/products', (req, res) => {
-    res.send('eliminando productos')
+//Ruta 4
+app.delete('/products/:id', (req, res) => {
+    const productFound = products.find(function (product) {
+        //return product.id == req.params.id 
+        return product.id === parseInt(req.params.id)
+    })
+
+    if(!productFound) 
+        return res.status(404).json({
+            message: "Product not found" 
+        })
+
+    products = products.filter(p => p.id !== parseInt(req.params.id))
+  
+    res.sendStatus(204)
 })
 
+//Ruta 5
 app.get('/products/:id', (req, res) => {
-    res.send('obteniendo un producto')
+    const productFound = products.find(function (product) {
+        //return product.id == req.params.id 
+        return product.id === parseInt(req.params.id)
+    })
+
+    if(!productFound) return res.status(404).json({ message: "Product not found" })
+
+    res.json(productFound)
 })
 
 
