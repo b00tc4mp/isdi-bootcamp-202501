1028d7b7e95c409facb66074d3158c41ecfa4095
manuelbarzi <manuelbarzi@gmail.com>
2025-03-21 11:17:30 +0100

    implement get-posts logic and test #40

--- staff/manuel-barzi/product/api/logic/getPosts.test.js
+++ staff/manuel-barzi/product/api/logic/getPosts.test.js
@@ -1,9 +1,19 @@
+import { data } from '../data/index.js'
 import { getPosts } from './getPosts.js'
 
-try {
-    const posts = getPosts('m7ypqlzdwy')
+console.info('TEST getPosts')
 
-    console.log(posts)
-} catch (error) {
-    console.error(error)
-}
+data.connect('mongodb://localhost:27017', 'test')
+    .then(() => {
+        try {
+            let posts2
+
+            return getPosts('67dd3aacae6eebd29ddf55c7')
+                .then(posts => posts2 = posts)
+                .finally(() => console.assert(posts2 instanceof Array, 'posts2 is an array'))
+        } catch (error) {
+            console.error(error)
+        }
+    })
+    .catch(error => console.error(error))
+    .finally(() => data.disconnect())
