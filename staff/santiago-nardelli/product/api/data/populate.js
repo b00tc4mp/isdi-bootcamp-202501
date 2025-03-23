a6077d1a958ac8fb7efe2c6e4e233f41b9523f27
Santiagon1996 <santiagoanardelli96@gmail.com>
2025-03-23 12:07:43 -0300

    modify my populate anda try some new config #50

--- staff/santiago-nardelli/product/api/data/populate.js
+++ staff/santiago-nardelli/product/api/data/populate.js
@@ -1,24 +1,50 @@
-import {MongoClient} from 'mongodb'
+import { MongoClient } from "mongodb";
 
-const client = new MongoClient('mongodb://localhost:27017')
+// Conectamos a la base de datos
+const client = new MongoClient("mongodb://localhost:27017");
 
-client.connect()
-    .then(()=>{
-        const db = client.db('test')
+client
+  .connect()
+  .catch((err) => {
+    console.error("Error al conectar a la base de datos:", err);
+    process.exit(1); // Salimos si hay un error crítico
+  })
+  .then(() => {
+    console.log("Conexión exitosa a la base de datos");
+    const db = client.db("test");
 
-        const users = db.collection('users')
-        const posts = db.collection('posts')
+    const users = db.collection("users");
 
-        return users.insertOne({
-            name:'Diego Maradona',
-            email:'diego@maradona.com',
-            password:'123456'
-        })
+    // Ejemplo: Insertar un usuario
+    return users
+      .insertOne({
+        name: "Julian Alvarez",
+        email: "julian@alvarez.com",
+        password: "123456",
+      })
+      .then((insertResult) => {
+        console.log("Usuario insertado:", insertResult);
 
-    })
-    .then(result=>{
-        console.log(result)
-    })
-    .finally(()=>{
-        client.close()
-    })
+        // Ejemplo: Buscar usuarios
+        return users.find().toArray();
+      })
+      .then((usersList) => {
+        console.log("Usuarios encontrados:", usersList);
+
+        // Ejemplo: Eliminar un usuario
+        return users.deleteOne({ name: "Arnau" });
+      })
+      .then((deleteResult) => {
+        console.log("Usuario eliminado:", deleteResult);
+      });
+  })
+  .then(() => {
+    console.log("Operaciones completadas exitosamente");
+  })
+  .catch((err) => {
+    console.error("Error durante las operaciones:", err);
+  })
+  .finally(() => {
+    client.close();
+    console.log("Conexión cerrada");
+  });
