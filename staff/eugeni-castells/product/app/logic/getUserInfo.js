5ba9b2b63f45f1332455fad4e51987afd72f03ff
Eugeni <ecastellsricart@gmail.com>
2025-03-23 19:24:38 +0100

    fix getUserName in app logic #57

--- staff/eugeni-castells/product/app/logic/getUserInfo.js
+++ staff/eugeni-castells/product/app/logic/getUserInfo.js
@@ -4,44 +4,39 @@ import { validate, errors } from "../../com";
 const { SystemError } = errors;
 
 export const getUserInfo = () => {
-  try {
-    const { userId } = data;
-    validate.id(userId, "user id");
+  const { userId } = data;
 
-    return fetch("http://localhost:8080/users/self", {
-      method: "GET",
-      headers: { Authorization: `${data.userId}` },
+  validate.id(userId, "user id");
+
+  return fetch("http://localhost:8080/users/self", {
+    method: "GET",
+    headers: { Authorization: `${data.userId}` },
+  })
+    .catch((error) => {
+      throw new SystemError(error.message);
     })
-      .catch((error) => {
-        throw new SystemError(error.message);
-      })
-      .then((response) => {
-        if (response.status === 200)
-          return response
-            .json()
-            .catch((error) => {
-              throw new S(error.message);
-            })
-            .then((body) => {
-              return body;
-            });
-
+    .then((response) => {
+      if (response.status === 200)
         return response
           .json()
           .catch((error) => {
-            throw new SystemError(error.message);
+            throw new S(error.message);
           })
           .then((body) => {
-            const { error, message } = body;
-
-            const constructor = errors[error];
-
-            throw new constructor(message);
+            return body;
           });
-      });
-  } catch (error) {
-    console.error(error);
 
-    alert(error.message);
-  }
+      return response
+        .json()
+        .catch((error) => {
+          throw new SystemError(error.message);
+        })
+        .then((body) => {
+          const { error, message } = body;
+
+          const constructor = errors[error];
+
+          throw new constructor(message);
+        });
+    });
 };
