04d746f07fe7e74fad4d849a812297e5fd29f973
Sergi <sergicpiqueras@gmail.com>
2025-03-26 14:42:48 +0100

    add scraping #5

--- staff/sergi-castellar/playground/scraping/scraping.py
+++
@@ -1,6 +0,0 @@
-from requests_html import HTMLSession
-
-s = HTMLSession()
-r = s.get('http://www.clubdelbarman-abecat.com/es/listado-cocteles/')
-cocktails = r.html.find('p', first=True)
-print cocktails
