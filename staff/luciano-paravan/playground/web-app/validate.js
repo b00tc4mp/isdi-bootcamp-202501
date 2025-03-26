a8cd558490bbf6267f844fc8dcfc560fe9b2bc24
lucianoparavanit <lucianoparavanit@gmail.com>
2025-03-26 16:57:36 +0100

    remove web-app files #47

--- staff/luciano-paravan/playground/web-app/validate.js
+++
@@ -1,8 +0,0 @@
-const validate = {
-    text(text, explain = 'text') {
-        if (typeof text !== 'string') throw new TypeError(`invalid ${explain} type`)
-        if (!text.length) throw new RangeError(`invalid ${explain} length`)
-    }
-}
-
-module.exports = validate
