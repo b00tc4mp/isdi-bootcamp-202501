e4f33a88a00ebb1537945b2a65f851ef50a7f985
Eugeni <ecastellsricart@gmail.com>
2025-03-27 11:58:33 +0100

    fic get-posts test sh so it returns posts, not error #57

--- staff/eugeni-castells/product/api/tests/get-posts.sh
+++ staff/eugeni-castells/product/api/tests/get-posts.sh
@@ -1,1 +1,1 @@
-curl -X GET http://localhost:8080/posts -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2N2RmZTQxNDhjMGY4YjIzMGVkOTE4MmEiLCJpYXQiOjE3NDI5MTMzNTR9.TCoNLaA_1fsgdmKKAX_BmIoZFoLe7k4AA4spTuKQc34' -v 
+curl -X GET http://localhost:8080/posts -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2N2UyZGIwMzJmYjc2NmZlMzNlYzQ2NTAiLCJpYXQiOjE3NDMwNjYzMjl9.jjXcfap-nuhVjffUu8TQTUuRwA2w6dovkoNSFFDZoc0' -v 
