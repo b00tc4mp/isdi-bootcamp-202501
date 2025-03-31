d7717f08b31c6bf9ececb8c2f0f0fdd6ab4cd004
Eugeni <ecastellsricart@gmail.com>
2025-03-31 20:27:56 +0200

    remove getonlineUserName logic #57

--- staff/eugeni-castells/product/api/logic/index.js
+++ staff/eugeni-castells/product/api/logic/index.js
@@ -2,7 +2,6 @@ import { addPost } from "./addPost.js";
 import { authenticateUser } from "./authenticateUser.js";
 import { deletePost } from "./deletePost.js";
 import { getOnlineUserInfo } from "./getOnlineUserInfo.js";
-import { getOnlineUserName } from "./getOnlineUserName.js";
 import { getPosts } from "./getPosts.js";
 import { registerUser } from "./registerUser.js";
 import { updatePostLikes } from "./updatePostLikes.js";
@@ -14,7 +13,6 @@ export const logic = {
   authenticateUser,
   deletePost,
   getOnlineUserInfo,
-  getOnlineUserName,
   getPosts,
   registerUser,
   updatePostLikes,
