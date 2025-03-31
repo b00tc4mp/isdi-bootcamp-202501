1da00cc5712a7c935d835bfb79e53cbc28cdb118
Sergi <sergicpiqueras@gmail.com>
2025-03-31 16:20:08 +0200

    update getPosts spec #39

--- staff/sergi-castellar/product/api/logic/getPosts.spec.js
+++ staff/sergi-castellar/product/api/logic/getPosts.spec.js
@@ -20,7 +20,6 @@ describe('getPosts', () => {
 
     it('succeeds at getting posts', () => {
         const userId = new ObjectId('67dd9ed19312d9e32d86590a')
-        let postId
         let posts2
 
         return Promise.all([
@@ -46,16 +45,15 @@ describe('getPosts', () => {
             .then(posts => posts2 = posts)
             .finally(() => {
                 const [post1, post2] = posts2
-                console.log('post1, post2 :>> ', post1, post2);
                 expect(post1.textDescription).to.equal('testing1')
                 expect(post2.textDescription).to.equal('testing2')
                 expect(posts2).to.have.lengthOf(2)
             })
     })
 
-    /*it('succeeds at removing like', () => {
+    it('fails at non-existing user', () => {
         const userId = new ObjectId('67dd9ed19312d9e32d86590a')
-        let postId
+        let catchedError
 
         return Promise.all([
             User.create({
@@ -68,53 +66,21 @@ describe('getPosts', () => {
             Post.create({
                 authorId: userId,
                 imageSrc: 'https://i.blogs.es/a19bfc/testing/450_1000.webp',
-                textDescription: 'testing',
-                likes: [userId]
+                textDescription: 'testing1',
+            }),
+            Post.create({
+                authorId: userId,
+                imageSrc: 'https://i.blogs.es/a19bfc/testing/450_1000.webp',
+                textDescription: 'testing2',
             })
         ])
-            .then(() => Post.findOne({ textDescription: 'testing' }).lean())
-            .then(post => {
-                postId = post._id
-                return getPosts(userId.toString(), postId.toString())
-            })
-            .then(() => Post.findById(postId.toString()).lean())
-            .then(post => {
-                expect(post.likes).to.not.include(userId.toString())
-                expect(post.likes).to.have.lengthOf(0)
-            })
-    })
-
-    it('fails on non existing user', () => {
-        let catchedError
-
-        return getPosts('67dd9ed19312d9e32d86590c', '67dd9ed19312d9e32d865913')
+            .then(() => getPosts('67dd9ed19312d9e32d86590c'))
             .catch(error => catchedError = error)
             .finally(() => {
                 expect(catchedError).to.be.instanceOf(NotFoundError)
                 expect(catchedError.message).to.equal('user not found')
             })
     })
-
-    it('fails on non existing post', () => {
-        let catchedError
-        const userId = new ObjectId('67dd9ed19312d9e32d86590a')
-
-        return User.create({
-            _id: userId,
-            name: 'prueba',
-            email: 'prueba@prueba.com',
-            username: 'prueba',
-            password: '$2b$10$.od0CuzCT6HHQdw/K3P/A.slG37zTbukraw1hLqa1r/TfBvY4C2mq'
-        })
-            .then(() => {
-                return getPosts(userId.toString(), '67dd9ed19312d9e32d865913')
-            })
-            .catch(error => catchedError = error)
-            .finally(() => {
-                expect(catchedError).to.be.instanceOf(NotFoundError)
-                expect(catchedError.message).to.equal('post not found')
-            })
-    })*/
 
     afterEach(() => {
         return Promise.all([
