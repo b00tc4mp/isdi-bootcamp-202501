bd9542d430ad4f68089d1310fc9f09e6dd6e2375
arnau-romero-hazas <aromerohazas@gmail.com>
2025-04-02 19:40:42 +0200

    add search an especific profile #51

--- staff/arnau-romero/product/app/view/Home/Search.jsx
+++ staff/arnau-romero/product/app/view/Home/Search.jsx
@@ -1,33 +1,41 @@
 import { useEffect } from 'react'
 import { useSearchParams } from 'react-router'
+import { Routes, Route, useNavigate, useLocation} from 'react-router'
+import { toast } from "react-hot-toast";
+import { logic } from '../../logic/index.js';
 
 export const Search = () => {
     const [searchParams, setSearchParams] = useSearchParams()
-    const query = searchParams.get('q')
-
-    useEffect(() => {
-        const query = searchParams.get('q')
-
-        console.log(' TODO call logic searchUsers with query', query)
-    }, [searchParams])
+    const navigate = useNavigate()
 
     const handleSearch = event => {
         event.preventDefault()
 
         const form = event.target
-
         const query = form.query.value
 
-        setSearchParams({ q : query })
+        if (!query.trim()) {
+            toast.error("Search query cannot be empty")
+            return
+        }
+
+        setSearchParams({ q: query })
+
+        try {
+            const userId = logic.getUserId()
+            navigate(`/${query}`, { state: { userId } }) // Ahora solo se ejecuta tras la búsqueda
+        } catch(error) {
+            console.error(error)
+            toast.error(error.message)
+        }
     }
 
     return <>
         <h1>Search</h1>
 
         <form onSubmit={handleSearch}>
-            <input type="text" name="query" id="query" defaultValue={query}  />
+            <input type="text" name="query" id="query" defaultValue={searchParams.get('q')} />
             <button type="submit">Search</button>
         </form>
     </>
-
 }
