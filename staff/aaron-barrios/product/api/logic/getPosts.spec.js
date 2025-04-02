0ece6a8929444cacf122d2082adad5c79b729fcf
Aarchyy89 <aaronbmaartin@gmail.com>
2025-04-02 15:39:14 +0200

    add chaiAsPromised, c8, test reports #42

--- staff/aaron-barrios/product/api/logic/getPosts.spec.js
+++ staff/aaron-barrios/product/api/logic/getPosts.spec.js
@@ -1,12 +1,18 @@
 import 'dotenv/config'
-import {data, User, Post} from '../data/index.js'
-import {getPosts} from './getPosts.js'
-import {expect} from 'chai'
-import {NotFoundError} from 'com/errors.js'
+import { data, User, Post } from '../data/index.js'
+import { getPosts } from './getPosts.js'
+import { NotFoundError, ValidationError } from 'com/errors.js'
+import { expect } from 'chai'
+import * as chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import {Types} from 'mongoose'
 
-const {MONGO_URL , MONGO_DB } = process.env
+chai.use(chaiAsPromised)
 
-describe('getPosts', () => {
+const { MONGO_URL, MONGO_DB } = process.env
+const {ObjectId} = Types
+
+describe.only('getPosts', () => {
     before(() => data.connect(MONGO_URL, MONGO_DB))
 
     debugger
@@ -18,35 +24,67 @@ describe('getPosts', () => {
         ])
     })
 
-    it.only('succeds on getPosts', () => {
+    it('succeds on getPosts', () => {
+        let returnedPosts
+        let user, user2
+        let post, post2
+
         return User.insertMany([
             { name: 'Diver Tido', email: 'diver@tido.com', username: 'diver', password: 'dididi' },
             { name: 'John Doe', email: 'john@doe.com', username: 'john', password: 'jojojo' }
         ])
-            .then(users => {
-                return Post.insertMany([
-                        { author: users[0].id, image: 'https://media.giphy.com/media/tqj4m9BRURayxQAIW9/giphy.gif?cid=790b7611hqe6rpquy1lo6acfy5cbmnkfaqmu4pn7mc77m2py&ep=v1_gifs_trending&rid=giphy.gif&ct=g', text: 'test'},
-                        { author: users[1].id, image: 'https://media.giphy.com/media/tqj4m9BRURayxQAIW9/giphy.gif?cid=790b7611hqe6rpquy1lo6acfy5cbmnkfaqmu4pn7mc77m2py&ep=v1_gifs_trending&rid=giphy.gif&ct=g', text: 'test2'},
-                    ])
+            .then(([_user, _user2]) => {
+                user = _user
+                user2 = _user2
             })
             .then(() => {
-                return Post.find().populate('author', 'id').lean()
+                return Post.insertMany([
+                    { author: user.id, image: 'https://media.giphy.com/media/tqj4m9BRURayxQAIW9/giphy.gif?cid=790b7611hqe6rpquy1lo6acfy5cbmnkfaqmu4pn7mc77m2py&ep=v1_gifs_trending&rid=giphy.gif&ct=g', text: 'post from user1', likes: [user2.id], createdAt: new Date(2025, 1, 11) },
+                    { author: user2.id, image: 'https://media.giphy.com/media/tqj4m9BRURayxQAIW9/giphy.gif?cid=790b7611hqe6rpquy1lo6acfy5cbmnkfaqmu4pn7mc77m2py&ep=v1_gifs_trending&rid=giphy.gif&ct=g', text: 'user2 post', likes: [user.id, user2.id], createdAt: new Date(2025, 1, 12), modifiedAt: new Date(2025, 1, 13) },
+                ])
             })
-            .then(posts => {
-                expect(posts).not.to.be.undefined
+            .then(([_post, _post2]) => {
+                    post = _post
+                    post2 = _post2
+                })
+            .then(() => getPosts(user2.id))
+            .then(posts => returnedPosts = posts)
+            .finally(() => {
+                expect(returnedPosts).to.be.instanceOf(Array)
+                expect(returnedPosts).to.have.lengthOf(2)
+
+                let returnedPost = returnedPosts[0]
+                expect(returnedPost.author.id).to.equal(user2.id)
+                expect(returnedPost.author.username).to.equal(user2.username)
+                expect(returnedPost.image).to.equal(post2.image)
+                expect(returnedPost.text).to.equal(post2.text)
+                expect(returnedPost.createdAt).to.deep.equal(post2.createdAt)
+                expect(returnedPost.modifiedAt).to.deep.equal(post2.modifiedAt)
+                expect(returnedPost.own).to.be.true
+                expect(returnedPost.liked).to.be.true
+                expect(returnedPost.likesCount).to.equal(2)
+
+                returnedPost = returnedPosts[1]
+                expect(returnedPost.author.id).to.equal(user.id)
+                expect(returnedPost.author.username).to.equal(user.username)
+                expect(returnedPost.image).to.equal(post.image)
+                expect(returnedPost.text).to.equal(post.text)
+                expect(returnedPost.createdAt).to.deep.equal(post.createdAt)
+                expect(returnedPost.modifiedAt).to.be.null
+                expect(returnedPost.own).to.be.false
+                expect(returnedPost.liked).to.be.true
+                expect(returnedPost.likesCount).to.equal(1)
             })
     })
 
     it('fails on existingUser', () => {
-        // debugger
-        let catchedError
+       return expect(getPosts(new ObjectId().toString())).to.be.rejectedWith(NotFoundError, 'User not found!')
+    })
 
-        return getPosts('67eaaf5f4e0ec2a2d569336d')
-            .catch(error => catchedError = error)
-            .finally(() => {
-                expect(catchedError).to.be.instanceOf(NotFoundError)
-                expect(catchedError.message).to.equal('User not found!')
-            })
+    it('fails on validates', () => {
+        expect(() => getPosts(123)).to.throw(ValidationError, 'invalid userId type')
+        expect(() => getPosts('123')).to.throw(ValidationError, 'invalid userId length')
+        expect(() => getPosts(' '.repeat(24))).to.throw(ValidationError, 'invalid userId syntax')
     })
 
     afterEach(() => {
