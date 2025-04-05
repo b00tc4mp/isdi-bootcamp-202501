db1333d5e44c986b7aba963561e84df2dbdb5c5b
Eugeni <ecastellsricart@gmail.com>
2025-04-05 19:47:18 +0200

    add routes in App index

--- staff/eugeni-castells/product/app/App.jsx
+++ staff/eugeni-castells/product/app/App.jsx
@@ -1,60 +1,113 @@
 import { useEffect, useState } from "react";
-import isUserConnected from "./logic/isUserConnected.js";
-import Home from "./view/Home/index.jsx";
+import { logic } from "./logic/index.js";
 import Landing from "./view/Landing.jsx";
 import Login from "./view/Login.jsx";
 import Register from "./view/Register.jsx";
+import Home from "./view/Home/index.jsx";
+import { Route, useNavigate, Routes, Navigate } from "react-router";
 
 function App() {
-  const [view, setView] = useState("landing");
+  const [loggedIn, setLoggedIn] = useState(null);
+  const [showLanding, setShowLanding] = useState(true);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const isUserLoggedIn = isUserConnected();
+    try {
+      const loggedIn = logic.isUserLoggedIn;
 
-    isUserLoggedIn && setView("home");
+      setLoggedIn(loggedIn);
+    } catch (error) {
+      console.error(error);
+
+      alert(error.message);
+    }
   }, []);
 
   const handleRegisterNavigation = () => {
-    setView("register");
+    navigate("/register");
+    setShowLanding(false);
   };
 
   const handleLoginNavigation = () => {
-    setView("login");
+    navigate("/login");
+    setShowLanding(false);
   };
 
   const handleLoginSuccess = () => {
-    setView("home");
+    navigate("/");
+    setLoggedIn(true);
+    setShowLanding(false);
   };
 
   const handleLogoutSuccess = () => {
-    setView("login");
+    navigate("/login");
+    setLoggedIn(false);
+    setShowLanding(false);
   };
 
   const handleRegisterSuccess = () => {
-    setView("login");
+    navigate("/login");
+    setShowLanding(false);
   };
+
+  console.log("render App");
 
   return (
     <>
-      {view === "landing" && (
-        <Landing
-          onRegisterNavigation={handleRegisterNavigation}
-          onLoginNavigation={handleLoginNavigation}
-        />
+      {loggedIn !== null && (
+        <Routes>
+          <Route
+            path="/landing"
+            element={
+              loggedIn ? (
+                <Navigate to="/" />
+              ) : (
+                <Landing
+                  onRegisterNavigation={handleRegisterNavigation}
+                  onLoginNavigation={handleLoginNavigation}
+                />
+              )
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              loggedIn ? (
+                <Navigate to="/" />
+              ) : (
+                <Register
+                  onLoginNavigation={handleLoginNavigation}
+                  onRegisterSuccess={handleRegisterSuccess}
+                />
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              loggedIn ? (
+                <Navigate to="/" />
+              ) : (
+                <Login
+                  onRegisterNavigation={handleRegisterNavigation}
+                  onLoginSuccess={handleLoginSuccess}
+                />
+              )
+            }
+          />
+          <Route
+            path="/*"
+            element={
+              loggedIn ? (
+                <Home onLogoutSuccess={handleLogoutSuccess} />
+              ) : (
+                <Navigate to={showLanding ? "/landing" : "/login"} />
+              )
+            }
+          />
+        </Routes>
       )}
-      {view === "register" && (
-        <Register
-          onLoginNavigation={handleLoginNavigation}
-          onRegisterSuccess={handleRegisterSuccess}
-        />
-      )}
-      {view === "login" && (
-        <Login
-          onRegisterNavigation={handleRegisterNavigation}
-          onLoginSuccess={handleLoginSuccess}
-        />
-      )}
-      {view === "home" && <Home onLogoutSuccess={handleLogoutSuccess} />}
     </>
   );
 }
