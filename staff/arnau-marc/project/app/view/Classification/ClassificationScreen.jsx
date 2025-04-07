bf05d8dd788487d7810836771c94d8f58467266a
Marc Ramos i Jordà <ramos13@MacBook-Air-de-Marc.local>
2025-04-07 16:35:47 +0200

    change name of ClassificationScreen, create LoginScreen, CreateGameScreen and RegisterScreen WIP #81

--- staff/arnau-marc/project/app/view/Classification/ClassificationScreen.jsx
+++ staff/arnau-marc/project/app/view/Classification/ClassificationScreen.jsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native'
 
-export default function RegisterScreen() {
+export default function ClassificationScreen() {
   return (
     <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-xl font-bold text-purple-600">Register Screen</Text>
+      <Text className="text-xl font-bold text-indigo-600">Classification Screen</Text>
     </View>
   )
 }
---
+++ staff/arnau-marc/project/app/view/Home/CreateGameScreen.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function CreateGameScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-xl font-bold text-yellow-600">Create Game Screen</Text>
+    </View>
+  )
+}
