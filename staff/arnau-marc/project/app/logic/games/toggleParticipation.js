56267895e7da0ccb0d19f1274aa4e8b30677e0ff
Marc Ramos i Jordà <ramos13@MacBook-Air-de-Marc.local>
2025-04-09 17:36:36 +0200

    create toggleParticipation and setGameWinner logics WIP #81

---
+++ staff/arnau-marc/project/app/logic/games/toggleParticipation.js
@@ -0,0 +1,37 @@
+import { data } from '../../data/index.js'
+//import { errors, validate } from 'com'
+
+//const { SystemError } = errors
+
+export const toggleParticipation = gameId => {
+  //  validate.id(gameId, 'gameId')
+
+    return data.token
+    .then(token => {
+        if (!token) {
+          throw new Error('Token not found')
+        }
+
+        return fetch(`http://localhost:8080/games/${gameId}/participation`, {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+    })
+        .catch(error => { throw new Error(error.message) })
+        .then(response => {
+            if (response.status === 204)
+                return
+
+            return response.json()
+                .catch(error => { throw new Error(error.message) })
+                .then(body => {
+                    const { error, message } = body
+
+                    const constructor = errors[error]
+
+                    throw new constructor(message)
+                })
+        })
+}
