1947a08b407bcba356f6288aa9ffad46d4f2685b
Marc Ramos i Jordà <ramos13@MacBook-Air-de-Marc.local>
2025-04-09 11:37:48 +0200

    create logic of logoutUser, isUserLoggedIn and update index #81

--- staff/arnau-marc/project/app/logic/index.js
+++ staff/arnau-marc/project/app/logic/index.js
@@ -1,7 +1,11 @@
+import { isUserLoggedIn } from "./isUserLoggedIn.js"
 import { loginUser } from "./loginUser.js"
+import { logoutUser } from "./logoutUser.js"
 import { registerUser } from "./registerUser.js"
 
 export const logic = {
     loginUser,
     registerUser,
+    isUserLoggedIn,
+    logoutUser
 }
