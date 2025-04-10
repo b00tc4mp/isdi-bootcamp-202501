798a6ff9171f2ae22f89441e1301593ac223bff2
dalle-31 <dallen-31@hotmail.com>
2025-04-10 17:26:01 +0200

    temporal changes #78

---
+++ staff/david-alonso/project/patata/index.js
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+import { data } from './data/index.js'
+
+const { PORT, MONGO_URL, MONGO_DB } = process.env
+
+data.connect(MONGO_URL, MONGO_DB)
+    .catch(console.error)
+    .then(() => {
+        const api = express()
+
+        api.use(cors())
+
+        api.get('/', (req, res) => res.semd('Hello, API!'))
+
+        // api.use('/users', users)
+
+        // api.use(errorHandler)
+
+        api.listen(PORT, () => console.log(`API running on port ${PORT}`))
+
+    })
