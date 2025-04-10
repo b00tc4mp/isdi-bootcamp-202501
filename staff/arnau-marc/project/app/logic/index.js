92b8af0c79e0f1e5e4093595bd3cd04fb6ce99c9
Marc Ramos i Jordà <ramos13@MacBook-Air-de-Marc.local>
2025-04-10 10:09:51 +0200

    create logic getUserId, and utils #81

--- staff/arnau-marc/project/app/logic/index.js
+++ staff/arnau-marc/project/app/logic/index.js
@@ -1,11 +1,12 @@
-import { getUsername } from "./getUsername.js"
-import { loginUser } from "./loginUser.js"
-import { logoutUser } from "./logoutUser.js"
-import { registerUser } from "./registerUser.js"
-import { isUserLoggedIn } from "./isUserLoggedIn.js"
+import { getUsername } from './getUsername.js'
+import { loginUser } from './loginUser.js'
+import { logoutUser } from './logoutUser.js'
+import { registerUser } from './registerUser.js'
+import { isUserLoggedIn } from './isUserLoggedIn.js'
+import { getUserId } from './getUserId.js'
 
-import { createGame } from "./games/createGame.js"
-import { getGames } from "./games/getGames.js"
+import { createGame } from './games/createGame.js'
+import { getGames } from './games/getGames.js'
 
 export const logic = {
     loginUser,
@@ -13,6 +14,7 @@ export const logic = {
     logoutUser,
     getUsername,
     isUserLoggedIn,
+    getUserId,
 
     createGame,
     getGames
