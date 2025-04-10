88b99345de4257ad6e9f02c1a3df8ca913acea05
JorgeLara0322 <jorge.lara2203g@gmail.com>
2025-04-11 01:17:03 +0200

    create com package with initial logic (WIP) #73

---
+++ staff/jorge-lara/project/com/errors.js
@@ -0,0 +1,39 @@
+class SystemError extends Error {
+    constructor(message) {
+        super(message);
+
+        this.name = this.constructor.name
+    }
+}
+
+class NotFoundError extends Error {
+    constructor(message) {
+        super(message);
+
+        this.name = this.constructor.name
+    }
+}
+class DuplicityError extends Error {
+    constructor(message) {
+        super(message);
+
+        this.name = this.constructor.name
+    }
+}
+
+class CredentialsError extends Error {
+    constructor(message) {
+        super(message);
+
+        this.name = this.constructor.name
+    }
+}
+
+const errors = {
+    SystemError,
+    NotFoundError,
+    DuplicityError,
+    CredentialsError
+}
+
+export default errors;
