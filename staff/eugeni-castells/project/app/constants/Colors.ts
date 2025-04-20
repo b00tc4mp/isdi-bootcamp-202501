0b68622d99bf6a374367c80794619e71a4a83c4a
Eugeni <ecastellsricart@gmail.com>
2025-04-20 16:59:11 +0200

    add Colors and Padding styles in constants #88

--- staff/eugeni-castells/project/app/constants/Colors.ts
+++ staff/eugeni-castells/project/app/constants/Colors.ts
@@ -1,20 +1,24 @@
 export const Colors = {
   light: {
     background: "#FEFCFB", // bg-color
-    text: "#11181C", // o el que vulguis per defecte
+    text: "#11181C",
     button: "#3B82F6", // button color
     buttonText: "#FFFFFF", // text del botó sobre fons blau
-    tint: "#2f95dc", // afegeix aquesta línia
+    tint: "#2f95dc",
     headingText: "#FFFFFF",
     tabItemBackground: "#FEFCFB",
     tabItemLine: "#191919",
     tabItemFocused: "#3B82F6",
+    inactiveDot: "#FEFCFB80",
+    activeDot: "#FFFFFF",
+    caruselItemTitle: "#FFFFFF",
+    caruselItemDescription: "#FFFFFF",
   },
   dark: {
     background: "#121212",
     text: "#FEFCFB",
     button: "#3B82F6",
     buttonText: "#FFFFFF",
-    tint: "#2f95dc", // afegeix aquesta línia
+    tint: "#2f95dc",
   },
 };
--- staff/eugeni-castells/project/app/constants/Paddings.ts
+++ staff/eugeni-castells/project/app/constants/Paddings.ts
@@ -1,3 +1,15 @@
+const baseRem = 16;
+
+export const spacing = {
+  rem: baseRem, // 1rem = 16
+  xs: baseRem * 0.25, // 4
+  sm: baseRem * 0.5, // 8
+  md: baseRem, // 16
+  lg: baseRem * 1.5, // 24
+  xl: baseRem * 2, // 32
+  xxl: baseRem * 3, // 48
+};
+
 export const Paddings = {
-  screenPadding: "2rem",
+  screenPadding: 2 * spacing.rem,
 };
